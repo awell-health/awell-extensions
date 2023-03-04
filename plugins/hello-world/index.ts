ee60fdeeb8ed485e8cbfadf1587d2ec992a79de2
Etienne Bömcke <etienne@awellhealth.com>
2023-03-04 16:22:05 +0100

    chore: add settings in hello world demo plugin

--- plugins/hello-world/index.ts
+++ plugins/hello-world/index.ts
@@ -1,11 +1,12 @@
 import { log } from './actions'
 import { type ActivityPlugin } from '../../lib/types'
+import { settings } from './settings'
 
 export const HelloWorld: ActivityPlugin = {
   key: 'hello-world',
   category: 'demo',
   title: 'Hello World !',
-  settings: {},
+  settings,
   actions: {
     log,
   },
