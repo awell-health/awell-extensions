f576ea09172613c4c13f119b62b5ce765316305f
Etienne Bömcke <etienne@awellhealth.com>
2023-03-09 14:36:56 +0100

    feat: activity events
    - use unstructured text for event instead of structured action enum as
    this offers more flexibility to extension developers

--- lib/types/ActivityEvent.ts
+++ lib/types/ActivityEvent.ts
@@ -1,6 +1,5 @@
 export interface ActivityEvent {
-  // TODO: Decide if we want to reuse the action enum here or give full flexibility to extension devs
-  action: string
+  text: string
   date: string
   error?: {
     category: 'BAD_REQUEST' | 'UNKNOWN_SERVER_ERROR'
