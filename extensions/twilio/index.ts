30eede264a708da0624dee1c281fd5b5bdbbf5c6
Mohsin Hayat <mohsinhayat104@gmail.com>
2023-03-09 16:28:10 +0500

    feat(): add icon and field details in Twilio configuration

--- extensions/twilio/index.ts
+++ extensions/twilio/index.ts
@@ -5,6 +5,8 @@ import { settings } from './settings'
 export const Twilio: Extension = {
   key: 'twilio',
   title: 'Twilio',
+  icon_url: 'https://www.vectorlogo.zone/logos/twilio/twilio-icon.svg',
+  description: 'Send SMS messages to your care flow stakeholders.',
   category: 'Notifications',
   actions: {
     smsNotification,
