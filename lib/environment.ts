da0265b5aa249cbfad6a3f5ef788d675f6bd98d9
Etienne Bömcke <etienne@awellhealth.com>
2023-03-10 18:06:31 +0100

    feat: register incoming webhooks route in web server
    - new route that accepts incoming webhooks, dispatches them
    to the relevant extension to extract data points and publishes
    resulting message to pubsub

--- lib/environment.ts
+++ lib/environment.ts
@@ -5,6 +5,9 @@ export const environment = {
   EXTENSION_ACTIVITY_COMPLETED_TOPIC:
     process.env.EXTENSION_ACTIVITY_COMPLETED_TOPIC ??
     'extension-activity-completed',
+  EXTENSION_WEBHOOK_RECEIVED_TOPIC:
+    process.env.EXTENSION_WEBHOOK_RECEIVED_TOPIC ??
+    'extension-webhook-received',
   PORT: Number(process.env.PORT ?? 3000),
   LOG_LEVEL: process.env.LOG_LEVEL ?? 'info',
   PRETTY_LOGS: Boolean(process.env.PRETTY_LOGS ?? false),
