63dd77c5af2b0e6b497590dc1b2a0e97ec9262f3
Etienne Bömcke <etienne@awellhealth.com>
2023-03-13 15:46:15 +0100

    chore(healthie): fix typos in extension config and settings

--- extensions/healthie/index.ts
+++ extensions/healthie/index.ts
@@ -6,8 +6,8 @@ import { webhooks } from './webhooks'
 export const Healthie: Extension = {
   key: 'healthie',
   category: 'integration',
-  title: 'Helthie',
-  description: '',
+  title: 'Healthie',
+  description: 'Connect to the Healthie API and receive webhooks',
   icon_url:
     'https://assets-global.website-files.com/5ff335a72af33d6ff0d8191f/6001d714ffd4c31ae0361055_healthie-logo-color.svg',
   settings,
