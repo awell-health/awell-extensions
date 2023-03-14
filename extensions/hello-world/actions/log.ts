90c54f416f51e85e386ca55bbd033b9b6b59a8bf
Etienne Bömcke <etienne@awellhealth.com>
2023-03-14 12:09:58 +0100

    feat(hello-world): add sample data point in hello world to suppor testing

--- extensions/hello-world/actions/log.ts
+++ extensions/hello-world/actions/log.ts
@@ -1,4 +1,9 @@
-import { type Field, FieldType, type Action } from '../../../lib/types'
+import {
+  type Field,
+  FieldType,
+  type Action,
+  type DataPointDefinition,
+} from '../../../lib/types'
 import { type settings } from '../settings'
 
 const fields = {
@@ -10,15 +15,31 @@ const fields = {
   },
 } satisfies Record<string, Field>
 
-export const log: Action<typeof fields, typeof settings> = {
+const dataPoints = {
+  hello: {
+    key: 'hello',
+    valueType: 'string',
+  },
+} satisfies Record<string, DataPointDefinition>
+
+export const log: Action<
+  typeof fields,
+  typeof settings,
+  keyof typeof dataPoints
+> = {
   key: 'log',
   category: 'demo',
   title: 'Log hello world',
   fields,
   previewable: true,
+  dataPoints,
   onActivityCreated: async (payload, onComplete): Promise<void> => {
     const { activity, fields, settings } = payload
     console.log('Hello world!', { activity, fields, settings })
-    await onComplete()
+    await onComplete({
+      data_points: {
+        hello: 'world',
+      },
+    })
   },
 }
