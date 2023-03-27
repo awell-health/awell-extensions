0689d7774e2beffb9c5765646952043d03510d37
Michal Grzelak <grzelakm@outlook.com>
2023-03-27 14:26:40 +0200

    implement create journal entry action

---
+++ extensions/healthie/actions/createJournalEntry.ts
@@ -0,0 +1,102 @@
+import {
+  type DataPointDefinition,
+  type Action,
+  type Field,
+} from '../../../lib/types'
+import { Category } from '../../../lib/types/marketplace'
+import { getSdk } from '../gql/sdk'
+import { initialiseClient } from '../graphqlClient'
+import { type settings } from '../settings'
+
+
+const fields = {
+
+} satisfies Record<string, Field>
+
+const dataPoints = {
+  journalEntryId: {
+    key: 'journalEntryId',
+    valueType: 'string',
+  },
+} satisfies Record<string, DataPointDefinition>
+
+export const createJournalEntry: Action<
+  typeof fields,
+  typeof settings,
+  keyof typeof dataPoints
+> = {
+  key: 'createJournalEntry',
+  category: Category.INTEGRATIONS,
+  title: 'Create journal entry',
+  description: 'Create journal entry in Healthie.',
+  fields,
+  dataPoints,
+  previewable: true,
+  onActivityCreated: async (payload, onComplete, onError): Promise<void> => {
+    const { settings } = payload
+    // const { id } = fields
+    try {
+      // if (isNil(id)) {
+      //   await onError({
+      //     events: [
+      //       {
+      //         date: new Date().toISOString(),
+      //         text: { en: 'Fields are missing' },
+      //         error: {
+      //           category: 'MISSING_FIELDS',
+      //           message: '`id` is missing',
+      //         },
+      //       },
+      //     ],
+      //   })
+      //   return;
+      // }
+
+      const client = initialiseClient(settings)
+      if (client !== undefined) {
+        const sdk = getSdk(client)
+
+
+        const { data } = await sdk.createJournalEntry({ input: {} })
+
+        const journalEntryId = data.createEntry?.entry?.id;
+
+        // TODO: verify if needed
+        await onComplete(
+          {
+            data_points: {
+              journalEntryId
+            }
+          }
+        )
+      } else {
+        await onError({
+          events: [
+            {
+              date: new Date().toISOString(),
+              text: { en: 'API client requires an API url and API key' },
+              error: {
+                category: 'MISSING_SETTINGS',
+                message: 'Missing api url or api key',
+              },
+            },
+          ],
+        })
+      }
+    } catch (err) {
+      const error = err as Error
+      await onError({
+        events: [
+          {
+            date: new Date().toISOString(),
+            text: { en: 'Healthie API reported an error' },
+            error: {
+              category: 'SERVER_ERROR',
+              message: error.message,
+            },
+          },
+        ],
+      })
+    }
+  },
+}
