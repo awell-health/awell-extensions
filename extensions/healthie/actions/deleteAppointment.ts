f37cc436c1b02b84734e4681512803bab3ae280f
Michal Grzelak <grzelakm@outlook.com>
2023-03-27 14:26:35 +0200

    feat(): implement delete/cancel appointment actions

---
+++ extensions/healthie/actions/deleteAppointment.ts
@@ -0,0 +1,94 @@
+import { isNil } from 'lodash'
+import {
+  FieldType,
+  type Action,
+  type Field,
+} from '../../../lib/types'
+import { Category } from '../../../lib/types/marketplace'
+import { getSdk } from '../gql/sdk'
+import { initialiseClient } from '../graphqlClient'
+import { type settings } from '../settings'
+
+
+const fields = {
+  id: {
+    id: 'id',
+    label: 'ID',
+    description: 'The id of the appointment in Healthie',
+    type: FieldType.STRING,
+    required: true,
+  },
+} satisfies Record<string, Field>
+
+export const deleteAppointment: Action<
+  typeof fields,
+  typeof settings
+> = {
+  key: 'deleteAppointment',
+  category: Category.INTEGRATIONS,
+  title: 'Delete appointment',
+  description: 'Delete appointment in Healthie.',
+  fields,
+  previewable: true,
+  onActivityCreated: async (payload, onComplete, onError): Promise<void> => {
+    const { fields, settings } = payload
+    const { id } = fields
+    try {
+      if (isNil(id)) {
+        await onError({
+          events: [
+            {
+              date: new Date().toISOString(),
+              text: { en: 'Fields are missing' },
+              error: {
+                category: 'MISSING_FIELDS',
+                message: '`id` is missing',
+              },
+            },
+          ],
+        })
+        return;
+      }
+
+      const client = initialiseClient(settings)
+      if (client !== undefined) {
+        const sdk = getSdk(client)
+
+
+        await sdk.deleteAppointment({
+          id
+        })
+
+
+        await onComplete()
+      } else {
+        await onError({
+          events: [
+            {
+              date: new Date().toISOString(),
+              text: { en: 'API client requires an API url and API key' },
+              error: {
+                category: 'MISSING_SETTINGS',
+                message: 'Missing api url or api key',
+              },
+            },
+          ],
+        })
+      }
+    } catch (err) {
+      const error = err as Error
+      await onError({
+        events: [
+          {
+            date: new Date().toISOString(),
+            text: { en: 'Healthie API reported an error' },
+            error: {
+              category: 'SERVER_ERROR',
+              message: error.message,
+            },
+          },
+        ],
+      })
+    }
+  },
+}
