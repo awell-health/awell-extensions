7f9c9fe80e7089f3315578383a92831bbff85f37
Etienne Bömcke <etienne@awellhealth.com>
2023-03-30 16:04:42 +0200

    fix: fix deadline exceed bug that crashes server on startup
    - add googleapis library in dependencies
    - use googleapis client to check if a subscription exists as it does
    not cause the deadline exceeded error
    - improve logging around extension registration
    - restore cal dot com extension

--- extensions/index.ts
+++ extensions/index.ts
@@ -1,7 +1,7 @@
 import { Awell } from './awell'
-// import { CalDotCom } from './calDotCom'
+import { CalDotCom } from './calDotCom'
 import { Healthie } from './healthie'
 import { HelloWorld } from './hello-world'
 import { Twilio } from './twilio'
 
-export const extensions = [Awell, HelloWorld, Healthie, Twilio]
+export const extensions = [Awell, CalDotCom, HelloWorld, Healthie, Twilio]
