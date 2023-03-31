b94629ea08bd26f2ce98344eba26113a65d5a4bf
Michal Grzelak <grzelakm@outlook.com>
2023-03-31 14:44:31 +0200

    feat(): fully implement create patient action

--- extensions/elation/__mocks__/client.ts
+++ extensions/elation/__mocks__/client.ts
@@ -3,6 +3,9 @@ const ElationAPIClientMock = jest.fn().mockImplementation((params) => {
     getPatient: jest.fn((params) => {
       return { first_name: 'First', last_name: 'Last' }
     }),
+    createPatient: jest.fn((params) => {
+      return { id: 1 }
+    }),
   };
 });
 
--- extensions/elation/actions/index.ts
+++ extensions/elation/actions/index.ts
@@ -1,2 +1,3 @@
 export * from './getPatient'
 export * from './updatePatient'
+export * from './createPatient'
