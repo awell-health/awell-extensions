71577f2b83d9799ff642554c3757ae0147633bfa
Michal Grzelak <grzelakm@outlook.com>
2023-04-03 10:44:04 +0200

    feat(): add supported fields as data points

--- extensions/elation/actions/getPatient.ts
+++ extensions/elation/actions/getPatient.ts
@@ -26,12 +26,80 @@ const fields = {
 const dataPoints = {
   first_name: {
     key: 'first_name',
-    valueType: 'string',
+    valueType: 'string'
   },
   last_name: {
     key: 'last_name',
-    valueType: 'string',
+    valueType: 'string'
   },
+  dob: {
+    key: 'dob',
+    valueType: 'date'
+  },
+  sex: {
+    key: 'sex',
+    valueType: 'string'
+  },
+  primary_physician: {
+    key: 'primary_physician',
+    valueType: 'number'
+  },
+  caregiver_practice: {
+    key: 'caregiver_practice',
+    valueType: 'number'
+  },
+  middle_name: {
+    key: 'middle_name',
+    valueType: 'string'
+  },
+  actual_name: {
+    key: 'actual_name',
+    valueType: 'string'
+  },
+  gender_identity: {
+    key: 'gender_identity',
+    valueType: 'string'
+  },
+  legal_gender_marker: {
+    key: 'legal_gender_marker',
+    valueType: 'string'
+  },
+  pronouns: {
+    key: 'pronouns',
+    valueType: 'string'
+  },
+  sexual_orientation: {
+    key: 'sexual_orientation',
+    valueType: 'string'
+  },
+  ssn: {
+    key: 'ssn',
+    valueType: 'string'
+  },
+  ethnicity: {
+    key: 'ethnicity',
+    valueType: 'string'
+  },
+  race: {
+    key: 'race',
+    valueType: 'string'
+  },
+  preferred_language: {
+    key: 'preferred_language',
+    valueType: 'string'
+  },
+  notes: {
+    key: 'notes',
+    valueType: 'string'
+  },
+  previous_first_name: {
+    key: 'previous_first_name',
+    valueType: 'string'
+  },
+  previous_last_name: {
+    key: 'previous_last_name',
+    valueType: 'string'
+  }
 } satisfies Record<string, DataPointDefinition>
 
 export const getPatient: Action<
@@ -64,6 +132,23 @@ export const getPatient: Action<
         data_points: {
           first_name: patientInfo.first_name,
           last_name: patientInfo.last_name,
+          dob: patientInfo.dob,
+          sex: patientInfo.sex,
+          primary_physician: String(patientInfo.primary_physician),
+          caregiver_practice: String(patientInfo.caregiver_practice),
+          middle_name: patientInfo.middle_name,
+          actual_name: patientInfo.actual_name,
+          gender_identity: patientInfo.gender_identity,
+          legal_gender_marker: patientInfo.legal_gender_marker,
+          pronouns: patientInfo.pronouns,
+          sexual_orientation: patientInfo.sexual_orientation,
+          ssn: patientInfo.ssn,
+          ethnicity: patientInfo.ethnicity,
+          race: patientInfo.race,
+          preferred_language: patientInfo.preferred_language,
+          notes: patientInfo.notes,
+          previous_first_name: patientInfo.previous_first_name,
+          previous_last_name: patientInfo.previous_last_name
         },
       })
     } catch (err) {
