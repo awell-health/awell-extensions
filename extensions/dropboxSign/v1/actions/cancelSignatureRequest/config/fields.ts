5b5454f9a28ed01c471c8bf8b1849517676d8cbf
Nick Hellemans <NickHellemans93@gmail.com>
2023-04-05 15:15:27 +0200

    chore(): add label for action fields

--- extensions/dropboxSign/v1/actions/cancelSignatureRequest/config/fields.ts
+++ extensions/dropboxSign/v1/actions/cancelSignatureRequest/config/fields.ts
@@ -3,7 +3,8 @@ import { type Field, FieldType } from '../../../../../../lib/types'
 export const fields = {
   signatureRequestId: {
     id: 'signatureRequestId',
-    label: 'The id of the incomplete SignatureRequest to cancel.',
+    label: 'Signature request ID',
+    description: 'The id of the incomplete SignatureRequest to cancel.',
     type: FieldType.STRING,
     required: true,
   },
--- extensions/dropboxSign/v1/actions/sendRequestReminder/config/fields.ts
+++ extensions/dropboxSign/v1/actions/sendRequestReminder/config/fields.ts
@@ -3,13 +3,15 @@ import { type Field, FieldType } from '../../../../../../lib/types'
 export const fields = {
   signatureRequestId: {
     id: 'signatureRequestId',
-    label: 'The id of the SignatureRequest to send a reminder for.',
+    label: 'Signature request ID',
+    description: 'The id of the SignatureRequest to send a reminder for.',
     type: FieldType.STRING,
     required: true,
   },
   signerEmailAddress: {
     id: 'signerEmailAddress',
-    label: 'The email address of the signer to send a reminder to.',
+    label: 'Signer email address',
+    description: 'The email address of the signer to send a reminder to.',
     type: FieldType.STRING,
     required: true,
   },
