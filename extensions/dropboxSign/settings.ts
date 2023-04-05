c40a8fa0fce75a955bab28d69a8881db5ebb0774
Nick Hellemans <NickHellemans93@gmail.com>
2023-04-05 17:04:25 +0200

    chore(): testmode for dropboxsign

--- extensions/dropboxSign/settings.ts
+++ extensions/dropboxSign/settings.ts
@@ -17,4 +17,12 @@ export const settings = {
     description:
       'The client id of the API app created in Dropbox Sign. Only required if want to use embedded signature requests.',
   },
+  testMode: {
+    label: 'Test mode',
+    key: 'testMode',
+    obfuscated: false,
+    required: false,
+    description:
+      'Set to "Yes" if you want to execute all API calls to DropboxSign in test mode. When test mode is enabled, signature requests will not be legally binding. When disabled, keep in mind that you must upgrade to a paid DropboxSign API plan to create signature requests via the extension.',
+  },
 } satisfies Record<string, Setting>
