5d16193bd3000f43bd124a8a54384b04efaceeb1
Nick Hellemans <NickHellemans93@gmail.com>
2023-04-07 09:00:45 +0200

    chore(): fix labels & descriptions of MessageBird actions

--- extensions/messagebird/v1/actions/sendSms/config/fields.ts
+++ extensions/messagebird/v1/actions/sendSms/config/fields.ts
@@ -3,21 +3,24 @@ import { type Field, FieldType, StringType } from '../../../../../../lib/types'
 export const fields = {
   originator: {
     id: 'originator',
-    label:
+    label: 'Originator',
+    description:
       'The sender of the message. This can be a telephone number (including country code) or an alphanumeric string.',
     type: FieldType.STRING,
     required: true,
   },
   recipient: {
     id: 'recipient',
-    label: 'The mobile number of the recipient.',
+    label: 'Recipient',
+    description: 'The mobile number of the recipient.',
     type: FieldType.STRING,
     stringType: StringType.PHONE,
     required: true,
   },
   body: {
     id: 'body',
-    label: 'The content of your message.',
+    label: 'Body',
+    description: 'The content of your message.',
     type: FieldType.TEXT,
     required: true,
   },
--- extensions/messagebird/v1/actions/sendWhatsAppMessage/config/fields.ts
+++ extensions/messagebird/v1/actions/sendWhatsAppMessage/config/fields.ts
@@ -3,13 +3,15 @@ import { type Field, FieldType, StringType } from '../../../../../../lib/types'
 export const fields = {
   from: {
     id: 'from',
-    label: 'The channel ID from which the message should be sent.',
+    label: 'From',
+    description: 'The channel ID from which the message should be sent.',
     type: FieldType.STRING,
     required: true,
   },
   to: {
     id: 'to',
-    label:
+    label: 'To',
+    description:
       'Either a channel-specific identifier for the receiver (e.g. MSISDN for SMS or WhatsApp channels), or the ID of a MessageBird Contact.',
     type: FieldType.STRING,
     stringType: StringType.PHONE,
@@ -17,7 +19,8 @@ export const fields = {
   },
   content: {
     id: 'content',
-    label: 'The content of your message.',
+    label: 'Content',
+    description: 'The content of your message.',
     type: FieldType.TEXT,
     required: true,
   },
