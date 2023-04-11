59d5b1db93ddf3c7df5deb0daf9519f5b43b6cf3
Michal Grzelak <grzelakm@outlook.com>
2023-04-11 20:31:41 +0200

    Elation appointment (#58)
    * feat(): add typing
    
    * feat(): implement create appointment
    
    * feat(): implement get appointment
    
    * feat(): refactor appointments
    
    * feat(): refactor and change numeric->string
    
    * feat(): refactor
    
    * chore(): format
    
    * feat(): cr changes
    
    * feat(): make secret obfuscated

---
+++ extensions/elation/actions/__tests__/createAppointment.ts
@@ -0,0 +1,38 @@
+import { appointmentExample } from '../../__mocks__/constants'
+import { createAppointment } from '../createAppointment'
+
+jest.mock('../../client')
+
+describe('Simple create appointment action', () => {
+  const onComplete = jest.fn()
+  const settings = {
+    client_id: 'clientId',
+    client_secret: 'clientSecret',
+    username: 'username',
+    password: 'password',
+    auth_url: 'authUrl',
+    base_url: 'baseUrl',
+  }
+
+  beforeEach(() => {
+    onComplete.mockClear()
+  })
+
+  test('Should return with correct data_points', async () => {
+    await createAppointment.onActivityCreated(
+      {
+        fields: {
+          ...appointmentExample,
+        },
+        settings,
+      } as any,
+      onComplete,
+      jest.fn()
+    )
+    expect(onComplete).toHaveBeenCalledWith({
+      data_points: {
+        appointmentId: '1',
+      },
+    })
+  })
+})
---
+++ extensions/elation/actions/getAppointment.ts
@@ -0,0 +1,151 @@
+import { ZodError } from 'zod'
+import {
+  FieldType,
+  type Action,
+  type DataPointDefinition,
+  type Field,
+} from '../../../lib/types'
+import { Category } from '../../../lib/types/marketplace'
+import { type settings } from '../settings'
+import { ElationAPIClient, makeDataWrapper } from '../client'
+import { fromZodError } from 'zod-validation-error'
+import { AxiosError } from 'axios'
+import { settingsSchema } from '../validation/settings.zod'
+import { numberId } from '../validation/generic.zod'
+
+const fields = {
+  appointmentId: {
+    id: 'appointmentId',
+    label: 'Appointment ID',
+    description: 'The appointment ID (a number)',
+    type: FieldType.STRING,
+    required: true,
+  },
+} satisfies Record<string, Field>
+
+const dataPoints = {
+  scheduledDate: {
+    key: 'scheduledDate',
+    valueType: 'date',
+  },
+  reason: {
+    key: 'reason',
+    valueType: 'string',
+  },
+  patientId: {
+    key: 'patientId',
+    valueType: 'string',
+  },
+  physicianId: {
+    key: 'physicianId',
+    valueType: 'string',
+  },
+  practiceId: {
+    key: 'practiceId',
+    valueType: 'string',
+  },
+  duration: {
+    key: 'duration',
+    valueType: 'string',
+  },
+  description: {
+    key: 'description',
+    valueType: 'string',
+  },
+  serviceLocationId: {
+    key: 'serviceLocationId',
+    valueType: 'string',
+  },
+  telehealthDetails: {
+    key: 'telehealthDetails',
+    valueType: 'string',
+  },
+} satisfies Record<string, DataPointDefinition>
+
+export const getAppointment: Action<
+  typeof fields,
+  typeof settings,
+  keyof typeof dataPoints
+> = {
+  key: 'getAppointment',
+  category: Category.INTEGRATIONS,
+  title: 'Get Appointment',
+  description: "Get appointment using elation's scheduling api.",
+  fields,
+  previewable: true,
+  dataPoints,
+  onActivityCreated: async (payload, onComplete, onError): Promise<void> => {
+    try {
+      const { base_url, ...settings } = settingsSchema.parse(payload.settings)
+      const appointmentId = numberId.parse(payload.fields.appointmentId)
+
+      // API Call should produce AuthError or something dif.
+      const api = new ElationAPIClient({
+        auth: settings,
+        baseUrl: base_url,
+        makeDataWrapper,
+      })
+      const appointment = await api.getAppointment(appointmentId)
+      await onComplete({
+        data_points: {
+          scheduledDate: appointment.scheduled_date,
+          reason: appointment.reason,
+          patientId: String(appointment.patient),
+          physicianId: String(appointment.physician),
+          practiceId: String(appointment.practice),
+          duration: String(appointment.duration),
+          description: appointment.description,
+          serviceLocationId: String(appointment.service_location?.id),
+          telehealthDetails: appointment.telehealth_details,
+        },
+      })
+    } catch (err) {
+      if (err instanceof ZodError) {
+        const error = fromZodError(err)
+        await onError({
+          events: [
+            {
+              date: new Date().toISOString(),
+              text: { en: error.message },
+              error: {
+                category: 'SERVER_ERROR',
+                message: error.message,
+              },
+            },
+          ],
+        })
+      } else if (err instanceof AxiosError) {
+        await onError({
+          events: [
+            {
+              date: new Date().toISOString(),
+              text: {
+                en: `${err.status ?? '(no status code)'} Error: ${err.message}`,
+              },
+              error: {
+                category: 'BAD_REQUEST',
+                message: `${err.status ?? '(no status code)'} Error: ${
+                  err.message
+                }`,
+              },
+            },
+          ],
+        })
+      } else {
+        const message = (err as Error).message
+        await onError({
+          events: [
+            {
+              date: new Date().toISOString(),
+              text: { en: message },
+              error: {
+                category: 'SERVER_ERROR',
+                message,
+              },
+            },
+          ],
+        })
+      }
+    }
+  },
+}
