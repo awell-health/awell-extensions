c6b0b3dfb6f06fbd6c5cb421fc96184c663ac17f
Nick Hellemans <NickHellemans93@gmail.com>
2023-04-11 14:37:30 +0200

    Update extensions/twilio/common/validation.ts
    Co-authored-by: Danijel Bjelajac <danijel@awellhealth.com>

--- extensions/twilio/common/validation.ts
+++ extensions/twilio/common/validation.ts
@@ -9,3 +9,4 @@ export const PhoneValidationSchema = z.custom<'Phone'>((val) => {
 export const MessageValidationSchema = z
   .string()
   .min(1, { message: 'Missing or empty message' })
+  .max(1600, { message: 'Message can not be longer than 1600 characters' })
