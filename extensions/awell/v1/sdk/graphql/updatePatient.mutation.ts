d6b51367948ee05e20901e79096b2f41624f1931
Nick Hellemans <NickHellemans93@gmail.com>
2023-04-12 14:35:49 +0200

    fix(awell-workflow): make sure update patient mutation returns success field

--- extensions/awell/v1/sdk/graphql/updatePatient.mutation.ts
+++ extensions/awell/v1/sdk/graphql/updatePatient.mutation.ts
@@ -1,6 +1,8 @@
 export const updatePatientMutation = `
 mutation UpdatePatient($input: UpdatePatientInput!) {
   updatePatient(input: $input) {
+    code
+    success
     patient {
       id
     }
