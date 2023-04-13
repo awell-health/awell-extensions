5b52a8d0de9bf5902e8e37daa24004c59cf7ed5f
Michal Grzelak <grzelakm@outlook.com>
2023-04-13 10:30:33 +0200

    feat(): add test to createTask

---
+++ extensions/healthie/actions/__tests__/createTask.ts
@@ -0,0 +1,48 @@
+import { getSdk } from '../../gql/sdk'
+import { mockGetSdk, mockGetSdkReturn } from '../../gql/__mocks__/sdk'
+import { createTask } from '../createTask'
+
+jest.mock('../../gql/sdk')
+jest.mock('../../graphqlClient')
+
+describe('createTask action', () => {
+  const onComplete = jest.fn()
+
+  beforeAll(() => {
+    ;(getSdk as jest.Mock).mockImplementation(mockGetSdk)
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('Should create a task', async () => {
+    await createTask.onActivityCreated(
+      {
+        activity: {
+          id: 'activity-id',
+        },
+        patient: { id: 'test-patient' },
+        fields: {
+          patientId: undefined,
+          assignToUserId: undefined,
+          content: 'content',
+          dueDate: undefined,
+        },
+        settings: {
+          apiKey: 'apiKey',
+          apiUrl: 'test-url',
+        },
+      },
+      onComplete,
+      jest.fn()
+    )
+
+    expect(mockGetSdkReturn.createTask).toHaveBeenCalled()
+    expect(onComplete).toHaveBeenCalledWith({
+      data_points: {
+        taskId: 'task-1',
+      },
+    })
+  })
+})
