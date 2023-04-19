689c9377cbce0533a888e050650b469a35692c09
Michal Grzelak <grzelakm@outlook.com>
2023-04-19 10:24:47 +0200

    feat(): add test cases for failed validation

--- extensions/healthie/actions/__tests__/getPatient.ts
+++ extensions/healthie/actions/__tests__/getPatient.ts
@@ -7,65 +7,132 @@ jest.mock('../../graphqlClient')
 
 describe('getPatient action', () => {
   const onComplete = jest.fn()
+  const DATE_MOCK = new Date()
 
   beforeAll(() => {
     const mockSdk = getSdk as jest.Mock
     mockSdk.mockImplementation(mockGetSdk)
+    jest.spyOn(global, 'Date').mockImplementation(() => DATE_MOCK)
   })
 
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
-  test.each([
-    { healthiePhone: '+1 (555) 555-1234', validatedPhone: '+15555551234' },
-    { healthiePhone: '+48 123 456 789', validatedPhone: '+48123456789' },
-    { healthiePhone: '(+48) 123 456/789', validatedPhone: '+48123456789' },
-  ])(
-    'Should parse phone to $validatedPhone when healthie returns $healthiePhone',
-    async ({ healthiePhone, validatedPhone }) => {
-      const returnValue = mockGetSdkReturn.getUser({})
-      mockGetSdkReturn.getUser.mockReturnValueOnce({
-        data: {
-          user: {
-            ...returnValue.data.user,
-            phone_number: healthiePhone,
+  describe('phone number validation', () => {
+    test.each([
+      { healthiePhone: '+1 (555) 555-1234', validatedPhone: '+15555551234' },
+      { healthiePhone: '+48 123 456 789', validatedPhone: '+48123456789' },
+      { healthiePhone: '(+48) 123 456/789', validatedPhone: '+48123456789' },
+    ])(
+      '$#. Should parse phone to "$validatedPhone" when healthie returns "$healthiePhone"',
+      async ({ healthiePhone, validatedPhone }) => {
+        const returnValue = mockGetSdkReturn.getUser({})
+        mockGetSdkReturn.getUser.mockReturnValueOnce({
+          data: {
+            user: {
+              ...returnValue.data.user,
+              phone_number: healthiePhone,
+            },
           },
-        },
-      })
+        })
 
-      await getPatient.onActivityCreated(
-        {
-          activity: {
-            id: 'activity-id',
+        await getPatient.onActivityCreated(
+          {
+            activity: {
+              id: 'activity-id',
+            },
+            patient: { id: 'test-patient' },
+            fields: {
+              patientId: 'patient-1',
+            },
+            settings: {
+              apiKey: 'apiKey',
+              apiUrl: 'test-url',
+            },
           },
-          patient: { id: 'test-patient' },
-          fields: {
-            patientId: 'patient-1',
-          },
-          settings: {
-            apiKey: 'apiKey',
-            apiUrl: 'test-url',
-          },
-        },
-        onComplete,
-        jest.fn()
-      )
+          onComplete,
+          jest.fn()
+        )
 
-      expect(onComplete).toHaveBeenCalledWith({
-        data_points: {
-          firstName: returnValue.data.user.first_name,
-          lastName: returnValue.data.user.last_name,
-          dob: returnValue.data.user.dob,
-          email: returnValue.data.user.email,
-          gender: returnValue.data.user.gender,
-          phoneNumber: healthiePhone,
-          validatedPhoneNumber: validatedPhone,
-          groupName: returnValue.data.user.user_group.name,
-          primaryProviderId: returnValue.data.user.dietitian_id,
-        },
-        events: undefined,
-      })
-    }
-  )
+        expect(onComplete).toHaveBeenCalledWith({
+          data_points: {
+            firstName: returnValue.data.user.first_name,
+            lastName: returnValue.data.user.last_name,
+            dob: returnValue.data.user.dob,
+            email: returnValue.data.user.email,
+            gender: returnValue.data.user.gender,
+            phoneNumber: healthiePhone,
+            validatedPhoneNumber: validatedPhone,
+            groupName: returnValue.data.user.user_group.name,
+            primaryProviderId: returnValue.data.user.dietitian_id,
+          },
+          events: undefined,
+        })
+      }
+    )
+
+    test.each([
+      { healthiePhone: '', validatedPhone: undefined },
+      { healthiePhone: undefined, validatedPhone: undefined },
+    ])(
+      '$#. Should log event and return "undefined" for validatedPhone when healthie returns "$healthiePhone"',
+      async ({ healthiePhone, validatedPhone }) => {
+        const returnValue = mockGetSdkReturn.getUser({})
+        mockGetSdkReturn.getUser.mockReturnValueOnce({
+          data: {
+            user: {
+              ...returnValue.data.user,
+              phone_number: healthiePhone as string,
+            },
+          },
+        })
+
+        await getPatient.onActivityCreated(
+          {
+            activity: {
+              id: 'activity-id',
+            },
+            patient: { id: 'test-patient' },
+            fields: {
+              patientId: 'patient-1',
+            },
+            settings: {
+              apiKey: 'apiKey',
+              apiUrl: 'test-url',
+            },
+          },
+          onComplete,
+          jest.fn()
+        )
+
+        expect(onComplete).toHaveBeenCalledWith({
+          data_points: {
+            firstName: returnValue.data.user.first_name,
+            lastName: returnValue.data.user.last_name,
+            dob: returnValue.data.user.dob,
+            email: returnValue.data.user.email,
+            gender: returnValue.data.user.gender,
+            phoneNumber: healthiePhone,
+            validatedPhoneNumber: validatedPhone,
+            groupName: returnValue.data.user.user_group.name,
+            primaryProviderId: returnValue.data.user.dietitian_id,
+          },
+          events: [
+            {
+              date: DATE_MOCK.toISOString(),
+              error: {
+                category: 'WRONG_DATA',
+                message:
+                  "Phone number from Healthie not stored because it isn't a valid E.164 phone number",
+              },
+              text: {
+                en: "Phone number from Healthie not stored because it isn't a valid E.164 phone number",
+              },
+            },
+          ],
+        })
+      }
+    )
+  })
 })
