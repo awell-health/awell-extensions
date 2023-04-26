190bcd8cadd38c7fc335ad2be4549a6d10edc7c9
Michal Grzelak <grzelakm@outlook.com>
2023-04-26 12:10:37 +0200

    refactor collection types

--- extensions/elation/types/generic.ts
+++ extensions/elation/types/generic.ts
@@ -4,3 +4,13 @@ export interface Find<T> {
   previous: null | string
   results: T
 }
+
+/**
+ * Elation returns up to 100 results in a single query
+ */
+export interface ElationCollection<T> {
+  count: number
+  next: null | string
+  previous: null | string
+  results: T[]
+}
