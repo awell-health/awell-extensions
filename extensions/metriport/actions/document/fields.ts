161d9e49d5d82ee41558f236aa8af2ba0d4d916b
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-05-15 11:20:42 -0400

    add metriport extension

--- extensions/metriport/actions/document/fields.ts
+++ extensions/metriport/actions/document/fields.ts
@@ -4,7 +4,7 @@ export const listFields = {
   patientId: {
     id: 'patientId',
     label: 'Patient ID',
-    description: 'The ID of the Patient for which to list available Documents',
+    description: 'The ID of the Patient for which to list their available Documents',
     type: FieldType.STRING,
     required: true,
   },
