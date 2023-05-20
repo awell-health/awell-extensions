caa47933b43462224eb94fea46c19e100c8e0ace
JB <jonathan@awellhealth.com>
2023-05-20 10:56:36 -0700

    removed webserver and necessary packages, added release CI

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,3 @@
-import { createLightship } from 'lightship'
-import { ExtensionServer } from './ExtensionServer'
 import { extensions } from '../extensions'
-import { webServer } from './WebServer'
-import { environment } from '../lib/environment'
 
-const start = async (): Promise<void> => {
-  const lightship = await createLightship()
-  const log = webServer.log
-  const extensionServer = new ExtensionServer({
-    log,
-  })
-  lightship.registerShutdownHandler(async () => {
-    log.info('Shutting down extension server')
-    await extensionServer.shutDown()
-    log.info('Shutting down web server')
-    await webServer.close()
-  })
-  try {
-    await webServer.listen({ port: environment.PORT, host: '::' })
-    log.info('Initialising extension server')
-    await extensionServer.init()
-    await extensions.reduce(async (register, extension) => {
-      await register
-      await extensionServer.registerExtension(extension)
-    }, Promise.resolve())
-    log.info('Extension registration completed successfully')
-    lightship.signalReady()
-  } catch (err) {
-    log.fatal(err, 'Extension server failed to start')
-    await lightship.shutdown()
-    process.exit(-1)
-  }
-}
-
-void start()
+export default extensions
