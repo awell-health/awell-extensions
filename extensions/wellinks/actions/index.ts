fb7ea04ae1fe501d5be2fc5da7e1610fb18c382a
Ashvin Pradhan-Shenoy <ashvin.ps@gmail.com>
2023-06-02 11:46:08 -0400

    feat(wellinks-extension): adds an action to check for Check In Overrides

--- extensions/wellinks/actions/index.ts
+++ extensions/wellinks/actions/index.ts
@@ -1,8 +1,11 @@
 import { checkForOverride } from "./checkForOverride/checkForOverride";
 import { checkForScheduledAppointment } from "./checkForScheduledAppointment/checkForScheduledAppointment";
 import { checkForChat } from "./checkForChat/checkForChat";
+import { checkForCheckInOverride } from "./checkForCheckInOverride/checkForCheckInOverride";
+
 export const actions = {
     checkForOverride,
     checkForScheduledAppointment,
-    checkForChat
+    checkForChat,
+    checkForCheckInOverride
 }
