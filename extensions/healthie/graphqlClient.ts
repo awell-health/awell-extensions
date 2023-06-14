30af478375e517fad3217b54f81c59d566536f95
Michal Grzelak <grzelakm@outlook.com>
2023-06-14 11:18:07 +0200

    feat(): implement error handling for healthie graphql

--- extensions/healthie/graphqlClient.ts
+++ extensions/healthie/graphqlClient.ts
@@ -1,4 +1,6 @@
 import { GraphQLClient } from 'graphql-request'
+import { isNil } from 'lodash'
+import { getHealthieErrorFromResponse, HealthieError } from './errors'
 import { type settings } from './settings'
 
 export const initialiseClient = (
@@ -10,6 +12,16 @@ export const initialiseClient = (
       headers: {
         AuthorizationSource: 'API',
         Authorization: `Basic ${apiKey}`,
+      },
+      responseMiddleware: (response) => {
+        if (response instanceof HealthieError) {
+          return
+        }
+
+        const errors = getHealthieErrorFromResponse(response)
+        if (!isNil(errors) && errors.length > 0) {
+          throw new HealthieError(errors)
+        }
       },
     })
   }
