43c505278b23febf979e291d7478a5fa13618fde
Michal Grzelak <grzelakm@outlook.com>
2023-06-30 11:34:51 +0200

    feat(): fix test

--- extensions/cmDotCom/v1/tests/validation.test.ts
+++ extensions/cmDotCom/v1/tests/validation.test.ts
@@ -2,6 +2,7 @@ import { FromNameValidationSchema } from '../validation'
 
 describe('From name validation', () => {
   test.each([
+    { value: '' },
     { value: '1' },
     { value: '123456789' },
     { value: '0123456789123456' },
@@ -13,13 +14,12 @@ describe('From name validation', () => {
     }).not.toThrow()
   })
 
-  test.each([
-    { value: '' },
-    { value: '01234567891234567' },
-    { value: 'abc123def456' },
-  ])('$#. Should throw when value equals "$value"', async ({ value }) => {
-    expect(() => {
-      FromNameValidationSchema.parse(value)
-    }).toThrow()
-  })
+  test.each([{ value: '01234567891234567' }, { value: 'abc123def456' }])(
+    '$#. Should throw when value equals "$value"',
+    async ({ value }) => {
+      expect(() => {
+        FromNameValidationSchema.parse(value)
+      }).toThrow()
+    }
+  )
 })
