bdf4b796e9b7502953ed4a4e1530cb735d0d12b8
Ashvin Pradhan-Shenoy <ashvin.ps@gmail.com>
2023-07-06 16:05:46 -0400

    feat(wellinks-extension): adds new action to index

--- extensions/wellinks/actions/index.ts
+++ extensions/wellinks/actions/index.ts
@@ -2,10 +2,12 @@ import { checkForOverride } from "./checkForOverride/checkForOverride";
 import { checkForScheduledAppointment } from "./checkForScheduledAppointment/checkForScheduledAppointment";
 import { checkForChat } from "./checkForChat/checkForChat";
 import { checkForCheckInOverride } from "./checkForCheckInOverride/checkForCheckInOverride";
+import { unenrollFromSendgrid } from "./unenrollFromSendgrid/unenrollFromSendgrid";
 
 export const actions = {
     checkForOverride,
     checkForScheduledAppointment,
     checkForChat,
-    checkForCheckInOverride
+    checkForCheckInOverride,
+    unenrollFromSendgrid
 }
