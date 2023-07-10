51ac4eafd24a67a4d7ce8df4fa6de7b4e3bb1aef
Michal Grzelak <grzelakm@outlook.com>
2023-07-10 20:39:25 +0200

    Healthie span tags (#182)
    * chore(): add cheerio
    
    * feat(): add html parsing and test
    
    * fix(): only want to support parsing the message if the message is clearly html (opening <>)
    
    ---------
    
    Co-authored-by: JB <jonathan@awellhealth.com>

--- extensions/healthie/actions/sendChatMessage.ts
+++ extensions/healthie/actions/sendChatMessage.ts
@@ -1,4 +1,5 @@
 import { isNil } from 'lodash'
+import { load as cheerioLoad } from 'cheerio'
 import {
   type DataPointDefinition,
   FieldType,
@@ -90,13 +91,36 @@ export const sendChatMessage: Action<
           return data.createConversation?.conversation
         }
 
+        /**
+         * We remove the <span> tags from any html because Healthie doesn't
+         * support parsing URLs that include Awell dynamic variables (which include
+         * <span> tags). This is a temporary workaround until Healthie implements
+         * a more complete fix.
+         */
+        let maybeParsedMessage: string | undefined
+        const isValidHTML = /^<([A-Za-z][A-Za-z0-9]*)\b[^>]*>(.*?)<\/\1>/i.test(
+          message ?? ''
+        )
+        if (isValidHTML) {
+          // Parse the HTML content using cheerio
+          const $ = cheerioLoad(message ?? '', undefined, false)
+          // Remove all <span> tags
+          $('span').replaceWith(function () {
+            return $(this).contents()
+          })
+          // Get the modified HTML content
+          maybeParsedMessage = $.html()
+        } else {
+          maybeParsedMessage = message
+        }
+
         const sendMessage = async (
           conversationId: string
         ): Promise<SendChatMessage> => {
           return await sdk.sendChatMessage({
             input: {
               conversation_id: conversationId,
-              content: message,
+              content: maybeParsedMessage,
               /**
                * Send the message in name of the specified provider.
                * If empty or blank, it defaults to the authenticated user.
