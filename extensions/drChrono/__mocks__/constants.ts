0b1627251f34d1858d3e10e38a8e9a910ce80103
Michal Grzelak <grzelakm@outlook.com>
2023-07-13 11:25:39 +0200

    feat(): add test constant

---
+++ extensions/drChrono/__mocks__/constants.ts
@@ -0,0 +1,13 @@
+import { type PatientInput } from '../types/patient'
+
+export const patientExample: PatientInput = {
+  first_name: 'Test',
+  last_name: 'Action',
+  email: 'patient@test.com',
+  gender: 'male',
+  doctor: 123,
+  date_of_birth: '1940-08-29',
+  race: 'asian',
+  preferred_language: 'eng',
+  ethnicity: 'blank',
+}
