c9319fcfef0f482d7a01289d90484638764f19d6
Michal Grzelak <grzelakm@outlook.com>
2023-07-13 12:20:32 +0200

    chore(): remove unnecessary comment

--- extensions/drChrono/types/patient.ts
+++ extensions/drChrono/types/patient.ts
@@ -3,10 +3,6 @@ import { type patientSchema } from '../validation/patient.zod'
 
 export type PatientInput = z.infer<typeof patientSchema>
 
-/**
- * There is a difference between `input` and `output` objects in DrChrono,
- * some fields are readonly (not in input), some have different structure
- */
 export interface PatientResponse extends PatientInput {
   id: number
   address: string
