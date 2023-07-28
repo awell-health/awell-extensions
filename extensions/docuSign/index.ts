fbc44e699fb2f29dab5a5b752a704e4632b49db4
Nick Hellemans <nick@awellhealth.com>
2023-07-28 12:25:08 +0200

    Update DocuSign logo

--- extensions/docuSign/index.ts
+++ extensions/docuSign/index.ts
@@ -9,7 +9,7 @@ import { settings } from './settings'
 export const DocuSign: Extension = {
   key: 'docuSign',
   title: 'DocuSign',
-  icon_url: 'https://www.vectorlogo.zone/logos/docusign/docusign-ar21.svg',
+  icon_url: 'https://res.cloudinary.com/da7x4rzl4/image/upload/v1690539849/Awell%20Extensions/6345a42c2a73234ee04669dd_sig-icon.svg',
   description:
     'DocuSign makes your business faster, simpler and more cost-efficient with electronic agreements. Agree with confidence, with intuitive signing experiences across virtually any device.',
   category: Category.DOCUMENT_MANAGEMENT,
