fd40604e12cd31059c3b5f4a55a3ba8b56a9ea69
Michal Grzelak <grzelakm@outlook.com>
2023-08-03 10:32:49 +0200

    feat(): refactor json validation

--- extensions/sendbird/v1/validation.ts
+++ extensions/sendbird/v1/validation.ts
@@ -2,7 +2,7 @@ import { isEmpty, isNil } from 'lodash'
 import { z } from 'zod'
 import { type Metadata } from './types'
 
-export const MetadataValidationSchema = z
+export const JsonStringValidationSchema = z
   .string()
   .transform((str, ctx): Metadata => {
     if (isNil(str) || isEmpty(str)) return {}
@@ -17,25 +17,7 @@ export const MetadataValidationSchema = z
       if (typeof parsedJson !== 'object' || Array.isArray(parsedJson)) {
         ctx.addIssue({
           code: 'custom',
-          message: 'metadata should be an object',
-        })
-        return z.NEVER
-      }
-
-      const values = Object.values(parsedJson)
-
-      if (values.length > 5) {
-        ctx.addIssue({
-          code: 'custom',
-          message: 'metadata should have maximum of five key-value items',
-        })
-        return z.NEVER
-      }
-
-      if (values.some((val) => typeof val !== 'string')) {
-        ctx.addIssue({
-          code: 'custom',
-          message: 'The value of each metadata key must be a string',
+          message: 'The value should represent an object',
         })
         return z.NEVER
       }
@@ -46,3 +28,52 @@ export const MetadataValidationSchema = z
       return z.NEVER
     }
   })
+
+export const MetadataValidationSchema = JsonStringValidationSchema.transform(
+  (jsonString, ctx): Metadata => {
+    if (isNil(jsonString) || isEmpty(jsonString)) return {}
+
+    try {
+      const values = Object.values(jsonString)
+
+      if (values.length > 5) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'JSON should have maximum of five key-value items',
+        })
+        return z.NEVER
+      }
+
+      if (values.some((val) => typeof val !== 'string')) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'The value of each JSON key must be a string',
+        })
+        return z.NEVER
+      }
+
+      if (values.some((val) => val.length > 190)) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'The value of each JSON key must not exceed 190 characters',
+        })
+        return z.NEVER
+      }
+
+      const keys = Object.keys(jsonString)
+
+      if (keys.some((key) => key.length > 128)) {
+        ctx.addIssue({
+          code: 'custom',
+          message: 'Each key of the JSON must not exceed 128 characters',
+        })
+        return z.NEVER
+      }
+
+      return jsonString
+    } catch (e) {
+      ctx.addIssue({ code: 'custom', message: 'Invalid JSON' })
+      return z.NEVER
+    }
+  }
+)
