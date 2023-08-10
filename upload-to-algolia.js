19cc4ed356a4ea752799c0db942107543546c8f1
Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
2023-08-10 09:52:54 +0200

    chore(): finalise workflow

--- upload-to-algolia.js
+++ upload-to-algolia.js
@@ -9,11 +9,30 @@ const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID || '';
 const ALGOLIA_ADMIN_KEY = process.env.ALGOLIA_ADMIN_KEY || '';
 const INDEX_NAME = 'awell_developers';
 
+const EXCLUDE_EXTENSIONS = ['awell-extensions', 'hello-world'];
+
+// Function to clear objects in Algolia
+async function clearObjects() {
+    const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY);
+    const index = client.initIndex(INDEX_NAME);
+  
+    try {
+      const { objectIDs } = await index.deleteBy({
+        filters: `space:Awell Extensions`,
+      });
+      console.log('Objects deleted:', objectIDs);
+    } catch (error) {
+      console.error('Error deleting objects:', error);
+      process.exit(1);
+    }
+  }
+
 // Function to read README file and extract data
 function readReadme(extensionDir) {
   const readmePath = path.join(extensionDir, 'README.md');
-  const content = fs.readFileSync(readmePath, 'utf8');
-  const { data } = matter(content);
+  const fileContent = fs.readFileSync(readmePath, 'utf8');
+  const { content, data } = matter(fileContent);
+  
   return {
     title: data.title || '',
     description: data.description || '',
@@ -30,20 +49,20 @@ function generateAlgoliaData() {
 
     return {
       objectID: extensionKey,
-      space: 'awell-extensions',
+      space: 'Awell Extensions',
       title: title,
       description: description,
       content: content,
       slug: `/awell-extensions/marketplace/${extensionKey}`,
     };
-  });
+  }).filter((extension) => EXCLUDE_EXTENSIONS.includes(extension.objectID) === false);));
 }
 
 // Upload data to Algolia
 async function uploadDataToAlgolia(data) {
   const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY);
   const index = client.initIndex(INDEX_NAME);
-  
+
   try {
     const { objectIDs } = await index.saveObjects(data);
     console.log('Objects added/updated:', objectIDs);
@@ -56,6 +75,7 @@ async function uploadDataToAlgolia(data) {
 // Main function to generate and upload data
 async function main() {
   const algoliaData = generateAlgoliaData();
+  await clearObjects();
   await uploadDataToAlgolia(algoliaData);
 }
 
