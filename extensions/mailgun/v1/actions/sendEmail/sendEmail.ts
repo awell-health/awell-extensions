677bbb3fe8c3756e5cdea507e752cffeb63e00b7
Michal Grzelak <grzelakm@outlook.com>
2023-08-16 12:24:41 +0200

    feat(): remove mailgun error boilerplate

--- extensions/mailgun/v1/actions/sendEmail/sendEmail.ts
+++ extensions/mailgun/v1/actions/sendEmail/sendEmail.ts
@@ -5,8 +5,6 @@ import { validateSettings, type settings } from '../../../settings'
 import mailgunSdk from '../../../common/sdk/mailgunSdk'
 import { getApiUrl } from '../../../common/utils'
 import { validateActionFields } from './config/fields'
-import { fromZodError } from 'zod-validation-error'
-import { ZodError } from 'zod'
 
 export const sendEmail: Action<typeof fields, typeof settings> = {
   key: 'sendEmail',
@@ -16,58 +14,24 @@ export const sendEmail: Action<typeof fields, typeof settings> = {
   fields,
   previewable: false,
   onActivityCreated: async (payload, onComplete, onError) => {
-    try {
-      const { to, subject, body } = validateActionFields(payload.fields)
-      const { apiKey, domain, region, fromName, fromEmail, testMode } =
-        validateSettings(payload.settings)
+    const { to, subject, body } = validateActionFields(payload.fields)
+    const { apiKey, domain, region, fromName, fromEmail, testMode } =
+      validateSettings(payload.settings)
 
-      const mg = mailgunSdk.client({
-        username: 'api',
-        key: apiKey,
-        url: getApiUrl({ region }),
-      })
+    const mg = mailgunSdk.client({
+      username: 'api',
+      key: apiKey,
+      url: getApiUrl({ region }),
+    })
 
-      await mg.messages.create(domain, {
-        from: `${fromName} <${fromEmail}>`,
-        to: [to],
-        subject,
-        html: body,
-        'o:testmode': testMode,
-      })
+    await mg.messages.create(domain, {
+      from: `${fromName} <${fromEmail}>`,
+      to: [to],
+      subject,
+      html: body,
+      'o:testmode': testMode,
+    })
 
-      await onComplete()
-    } catch (err) {
-      if (err instanceof ZodError) {
-        const error = fromZodError(err)
-        console.log(error.message)
-        await onError({
-          events: [
-            {
-              date: new Date().toISOString(),
-              text: { en: error.name },
-              error: {
-                category: 'WRONG_INPUT',
-                message: `${error.message}`,
-              },
-            },
-          ],
-        })
-        return
-      }
-
-      const error = err as Error
-      await onError({
-        events: [
-          {
-            date: new Date().toISOString(),
-            text: { en: 'Something went wrong while orchestration the action' },
-            error: {
-              category: 'SERVER_ERROR',
-              message: error.message,
-            },
-          },
-        ],
-      })
-    }
+    await onComplete()
   },
 }
