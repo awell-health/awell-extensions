98b0abffb0e0f3fe818c8585695e7d6defe60b4c
Michal Grzelak <grzelakm@outlook.com>
2023-08-16 12:10:32 +0200

    feat(): remove formsort error boilerplate

--- extensions/formsort/v1/actions/completeFlow/completeFlow.ts
+++ extensions/formsort/v1/actions/completeFlow/completeFlow.ts
@@ -3,8 +3,6 @@ import { fields } from './config'
 import { Category } from '@awell-health/extensions-core'
 import { type settings } from '../../../settings'
 import { validateActionFields } from './config/fields'
-import { fromZodError } from 'zod-validation-error'
-import { ZodError } from 'zod'
 
 export const completeFlow: Action<typeof fields, typeof settings> = {
   key: 'completeFlow',
@@ -24,36 +22,10 @@ export const completeFlow: Action<typeof fields, typeof settings> = {
     try {
       validateActionFields(payload.fields)
     } catch (err) {
-      if (err instanceof ZodError) {
-        const error = fromZodError(err)
-        await onError({
-          events: [
-            {
-              date: new Date().toISOString(),
-              text: { en: error.name },
-              error: {
-                category: 'WRONG_INPUT',
-                message: `${error.message}`,
-              },
-            },
-          ],
-        })
-        return
-      }
-
-      const error = err as Error
-      await onError({
-        events: [
-          {
-            date: new Date().toISOString(),
-            text: { en: 'Something went wrong while orchestration the action' },
-            error: {
-              category: 'SERVER_ERROR',
-              message: error.message,
-            },
-          },
-        ],
-      })
+      /**
+       * re-throw to be handled inside awell-extension-server
+       */
+      throw err
     }
   },
 }
