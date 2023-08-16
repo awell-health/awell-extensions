2847e21f06fbfc10dfca4c307245a60b308ddc1f
Michal Grzelak <grzelakm@outlook.com>
2023-08-16 12:23:55 +0200

    feat(): remove mailchimp error boilerplate

--- extensions/mailchimp/v1/actions/sendEmail/sendEmail.ts
+++ extensions/mailchimp/v1/actions/sendEmail/sendEmail.ts
@@ -1,10 +1,9 @@
 import { type Action } from '@awell-health/extensions-core'
 import { fields } from './config'
-import { Category , validate } from '@awell-health/extensions-core'
+import { Category, validate } from '@awell-health/extensions-core'
 import { SettingsValidationSchema, type settings } from '../../../settings'
 import { FieldsValidationSchema } from './config/fields'
-import { fromZodError } from 'zod-validation-error'
-import { z, ZodError } from 'zod'
+import { z } from 'zod'
 import mailchimpSdk from '../../../common/sdk/mailchimpSdk'
 
 export const sendEmail: Action<typeof fields, typeof settings> = {
@@ -15,72 +14,39 @@ export const sendEmail: Action<typeof fields, typeof settings> = {
   fields,
   previewable: false,
   onActivityCreated: async (payload, onComplete, onError) => {
-    try {
-      const {
-        patient: { id: patientId },
-        activity: { id: activityId },
-      } = payload
+    const {
+      patient: { id: patientId },
+      activity: { id: activityId },
+    } = payload
 
-      const {
-        fields: { to, subject, body },
-        settings: { apiKey, fromName, fromEmail },
-      } = validate({
-        schema: z.object({
-          fields: FieldsValidationSchema,
-          settings: SettingsValidationSchema,
-        }),
-        payload,
-      })
+    const {
+      fields: { to, subject, body },
+      settings: { apiKey, fromName, fromEmail },
+    } = validate({
+      schema: z.object({
+        fields: FieldsValidationSchema,
+        settings: SettingsValidationSchema,
+      }),
+      payload,
+    })
 
-      const apiClient = mailchimpSdk(apiKey)
+    const apiClient = mailchimpSdk(apiKey)
 
-      await apiClient.messages.send({
-        message: {
-          from_email: fromEmail,
-          from_name: fromName,
-          to: [{ email: to }],
-          subject,
-          html: body,
-          metadata: {
-            website: 'https://awell.health',
-            awellPatientId: patientId,
-            awellActivityId: activityId,
-          },
+    await apiClient.messages.send({
+      message: {
+        from_email: fromEmail,
+        from_name: fromName,
+        to: [{ email: to }],
+        subject,
+        html: body,
+        metadata: {
+          website: 'https://awell.health',
+          awellPatientId: patientId,
+          awellActivityId: activityId,
         },
-      })
+      },
+    })
 
-      await onComplete()
-    } catch (err) {
-      if (err instanceof ZodError) {
-        const error = fromZodError(err)
-        await onError({
-          events: [
-            {
-              date: new Date().toISOString(),
-              text: { en: error.name },
-              error: {
-                category: 'WRONG_INPUT',
-                message: `${error.message}`,
-              },
-            },
-          ],
-        })
-        return
-      }
-
-      const error = err as Error
-      await onError({
-        events: [
-          {
-            date: new Date().toISOString(),
-            text: { en: 'Something went wrong while orchestration the action' },
-            error: {
-              category: 'SERVER_ERROR',
-              message: error.message,
-            },
-          },
-        ],
-      })
-    }
+    await onComplete()
   },
 }
--- extensions/mailchimp/v1/actions/sendEmailWithTemplate/sendEmailWithTemplate.ts
+++ extensions/mailchimp/v1/actions/sendEmailWithTemplate/sendEmailWithTemplate.ts
@@ -1,11 +1,10 @@
 import { type Action } from '@awell-health/extensions-core'
 import { fields } from './config'
-import { Category , validate } from '@awell-health/extensions-core'
+import { Category, validate } from '@awell-health/extensions-core'
 import { SettingsValidationSchema, type settings } from '../../../settings'
 import mailchimpSdk from '../../../common/sdk/mailchimpSdk'
 import { FieldsValidationSchema } from './config/fields'
-import { fromZodError } from 'zod-validation-error'
-import { z, ZodError } from 'zod'
+import { z } from 'zod'
 
 export const sendEmailWithTemplate: Action<typeof fields, typeof settings> = {
   key: 'sendEmailWithTemplate',
@@ -16,74 +15,40 @@ export const sendEmailWithTemplate: Action<typeof fields, typeof settings> = {
   fields,
   previewable: false,
   onActivityCreated: async (payload, onComplete, onError) => {
-    try {
-      const {
-        patient: { id: patientId },
-        activity: { id: activityId },
-      } = payload
+    const {
+      patient: { id: patientId },
+      activity: { id: activityId },
+    } = payload
 
-      const {
-        fields: { to, subject, templateName, templateContent },
-        settings: { apiKey, fromName, fromEmail },
-      } = validate({
-        schema: z.object({
-          fields: FieldsValidationSchema,
-          settings: SettingsValidationSchema,
-        }),
-        payload,
-      })
+    const {
+      fields: { to, subject, templateName, templateContent },
+      settings: { apiKey, fromName, fromEmail },
+    } = validate({
+      schema: z.object({
+        fields: FieldsValidationSchema,
+        settings: SettingsValidationSchema,
+      }),
+      payload,
+    })
 
-      const apiClient = mailchimpSdk(apiKey)
+    const apiClient = mailchimpSdk(apiKey)
 
-      await apiClient.messages.sendTemplate({
-        template_name: templateName,
-        template_content: templateContent,
-        message: {
-          from_email: fromEmail,
-          from_name: fromName,
-          to: [{ email: to }],
-          subject,
-          metadata: {
-            website: 'https://awell.health',
-            awellPatientId: patientId,
-            awellActivityId: activityId,
-          },
+    await apiClient.messages.sendTemplate({
+      template_name: templateName,
+      template_content: templateContent,
+      message: {
+        from_email: fromEmail,
+        from_name: fromName,
+        to: [{ email: to }],
+        subject,
+        metadata: {
+          website: 'https://awell.health',
+          awellPatientId: patientId,
+          awellActivityId: activityId,
         },
-      })
+      },
+    })
 
-      await onComplete()
-    } catch (err) {
-      if (err instanceof ZodError) {
-        const error = fromZodError(err)
-        console.log(error.message)
-        await onError({
-          events: [
-            {
-              date: new Date().toISOString(),
-              text: { en: error.name },
-              error: {
-                category: 'WRONG_INPUT',
-                message: error.message,
-              },
-            },
-          ],
-        })
-        return
-      }
-
-      const error = err as Error
-      await onError({
-        events: [
-          {
-            date: new Date().toISOString(),
-            text: { en: 'Something went wrong while orchestration the action' },
-            error: {
-              category: 'SERVER_ERROR',
-              message: error.message,
-            },
-          },
-        ],
-      })
-    }
+    await onComplete()
   },
 }
