362fb9fcddc5d91d5e721cd606516fafef8bfa2d
Michal Grzelak <grzelakm@outlook.com>
2023-08-16 11:30:56 +0200

    feat(): remove calDotCom error boilerplate

--- extensions/calDotCom/actions/getBooking.ts
+++ extensions/calDotCom/actions/getBooking.ts
@@ -6,8 +6,6 @@ import {
   Category,
   validate,
 } from '@awell-health/extensions-core'
-import { ZodError } from 'zod'
-import { fromZodError } from 'zod-validation-error'
 import CalComApi from '../calComApi'
 import { GetBookingPayloadSchema } from '../schema'
 import { type settings } from '../settings'
@@ -65,61 +63,27 @@ export const getBooking: Action<typeof fields, typeof settings> = {
   dataPoints,
   previewable: false,
   onActivityCreated: async (payload, onComplete, onError) => {
-    try {
-      const {
-        fields: { bookingId },
-        settings: { apiKey },
-      } = validate({
-        schema: GetBookingPayloadSchema,
-        payload,
-      })
-      const calComApi = new CalComApi(apiKey)
-      const booking = await calComApi.getBooking(bookingId)
+    const {
+      fields: { bookingId },
+      settings: { apiKey },
+    } = validate({
+      schema: GetBookingPayloadSchema,
+      payload,
+    })
+    const calComApi = new CalComApi(apiKey)
+    const booking = await calComApi.getBooking(bookingId)
 
-      await onComplete({
-        data_points: {
-          eventTypeId: `${booking.eventTypeId}`,
-          title: booking.title,
-          description: booking.description,
-          startTime: booking.startTime,
-          endTime: booking.endTime,
-          status: booking.status,
-          cancelUrl: `https://app.cal.com/booking/${booking.uid}?cancel=true`,
-          rescheduleUrl: `https://app.cal.com/reschedule/${booking.uid}`,
-        },
-      })
-    } catch (error) {
-      if (error instanceof ZodError) {
-        const err = fromZodError(error)
-        await onError({
-          events: [
-            {
-              date: new Date().toISOString(),
-              text: { en: err.message },
-              error: {
-                category: 'BAD_REQUEST',
-                message: err.message,
-              },
-            },
-          ],
-        })
-        return
-      }
-      const errorMessage = (error as Error).message
-      await onError({
-        events: [
-          {
-            date: new Date().toISOString(),
-            text: {
-              en: `Get Booking failed: ${errorMessage}`,
-            },
-            error: {
-              category: 'SERVER_ERROR',
-              message: `Get Booking failed: ${errorMessage}`,
-            },
-          },
-        ],
-      })
-    }
+    await onComplete({
+      data_points: {
+        eventTypeId: `${booking.eventTypeId}`,
+        title: booking.title,
+        description: booking.description,
+        startTime: booking.startTime,
+        endTime: booking.endTime,
+        status: booking.status,
+        cancelUrl: `https://app.cal.com/booking/${booking.uid}?cancel=true`,
+        rescheduleUrl: `https://app.cal.com/reschedule/${booking.uid}`,
+      },
+    })
   },
 }
