a17d338ce1eec5f8a367c28760c30650c16238be
Michal Grzelak <grzelakm@outlook.com>
2023-08-16 12:25:36 +0200

    feat(): remove math error boilerplate

--- extensions/math/v1/actions/calculateDateDifference/calculateDateDifference.ts
+++ extensions/math/v1/actions/calculateDateDifference/calculateDateDifference.ts
@@ -1,9 +1,8 @@
 import { type Action } from '@awell-health/extensions-core'
-import { Category , validate } from '@awell-health/extensions-core'
+import { Category, validate } from '@awell-health/extensions-core'
 import { type settings } from '../../../settings'
 import { fields, dataPoints, FieldsValidationSchema } from './config'
-import { fromZodError } from 'zod-validation-error'
-import { z, ZodError } from 'zod'
+import { z } from 'zod'
 import {
   differenceInSeconds,
   differenceInMinutes,
@@ -25,84 +24,51 @@ export const calculateDateDifference: Action<typeof fields, typeof settings> = {
   dataPoints,
   previewable: true,
   onActivityCreated: async (payload, onComplete, onError): Promise<void> => {
-    try {
-      const {
-        fields: { dateLeft, dateRight, unit },
-      } = validate({
-        schema: z.object({ fields: FieldsValidationSchema }),
-        payload,
-      })
+    const {
+      fields: { dateLeft, dateRight, unit },
+    } = validate({
+      schema: z.object({ fields: FieldsValidationSchema }),
+      payload,
+    })
 
-      const calculateDifference = (): number => {
-        switch (unit) {
-          case 'seconds': {
-            return differenceInSeconds(dateLeft, dateRight)
-          }
-          case 'minutes': {
-            return differenceInMinutes(dateLeft, dateRight)
-          }
-          case 'hours': {
-            return differenceInHours(dateLeft, dateRight)
-          }
-          case 'days': {
-            return differenceInDays(dateLeft, dateRight)
-          }
-          case 'weeks': {
-            return differenceInWeeks(dateLeft, dateRight)
-          }
-          case 'months': {
-            return differenceInMonths(dateLeft, dateRight)
-          }
-          case 'years': {
-            return differenceInYears(dateLeft, dateRight)
-          }
-          default: {
-            throw new AssertionError({
-              message:
-                "`unit` should be captured by validation. We shouldn't reach this error.",
-            })
-          }
+    const calculateDifference = (): number => {
+      switch (unit) {
+        case 'seconds': {
+          return differenceInSeconds(dateLeft, dateRight)
+        }
+        case 'minutes': {
+          return differenceInMinutes(dateLeft, dateRight)
+        }
+        case 'hours': {
+          return differenceInHours(dateLeft, dateRight)
+        }
+        case 'days': {
+          return differenceInDays(dateLeft, dateRight)
+        }
+        case 'weeks': {
+          return differenceInWeeks(dateLeft, dateRight)
+        }
+        case 'months': {
+          return differenceInMonths(dateLeft, dateRight)
+        }
+        case 'years': {
+          return differenceInYears(dateLeft, dateRight)
+        }
+        default: {
+          throw new AssertionError({
+            message:
+              "`unit` should be captured by validation. We shouldn't reach this error.",
+          })
         }
       }
-
-      const dateDifference = calculateDifference()
-
-      await onComplete({
-        data_points: {
-          dateDifference: String(dateDifference),
-        },
-      })
-    } catch (err) {
-      if (err instanceof ZodError) {
-        const error = fromZodError(err)
-        await onError({
-          events: [
-            {
-              date: new Date().toISOString(),
-              text: { en: error.name },
-              error: {
-                category: 'WRONG_INPUT',
-                message: `${error.message}`,
-              },
-            },
-          ],
-        })
-        return
-      }
-
-      const error = err as Error
-      await onError({
-        events: [
-          {
-            date: new Date().toISOString(),
-            text: { en: 'Something went wrong while orchestrating the action' },
-            error: {
-              category: 'SERVER_ERROR',
-              message: error.message,
-            },
-          },
-        ],
-      })
     }
+
+    const dateDifference = calculateDifference()
+
+    await onComplete({
+      data_points: {
+        dateDifference: String(dateDifference),
+      },
+    })
   },
 }
--- extensions/math/v1/actions/generateRandomNumber/generateRandomNumber.ts
+++ extensions/math/v1/actions/generateRandomNumber/generateRandomNumber.ts
@@ -1,10 +1,9 @@
 import { type Action } from '@awell-health/extensions-core'
-import { Category , validate } from '@awell-health/extensions-core'
+import { Category, validate } from '@awell-health/extensions-core'
 import { type settings } from '../../../settings'
 import { fields, dataPoints } from './config'
 import { FieldsValidationSchema } from './config/fields'
-import { fromZodError } from 'zod-validation-error'
-import { z, ZodError } from 'zod'
+import { z } from 'zod'
 
 export const generateRandomNumber: Action<typeof fields, typeof settings> = {
   key: 'generateRandomNumber',
@@ -15,56 +14,23 @@ export const generateRandomNumber: Action<typeof fields, typeof settings> = {
   dataPoints,
   previewable: true,
   onActivityCreated: async (payload, onComplete, onError): Promise<void> => {
-    try {
-      const {
-        fields: { min, max },
-      } = validate({
-        schema: z.object({ fields: FieldsValidationSchema }),
-        payload,
-      })
+    const {
+      fields: { min, max },
+    } = validate({
+      schema: z.object({ fields: FieldsValidationSchema }),
+      payload,
+    })
 
-      const [minSerialized, maxSerialized] = [min, max].sort((a, b) => a - b)
+    const [minSerialized, maxSerialized] = [min, max].sort((a, b) => a - b)
 
-      const generatedNumber =
-        Math.floor(Math.random() * (maxSerialized - minSerialized + 1)) +
-        minSerialized
+    const generatedNumber =
+      Math.floor(Math.random() * (maxSerialized - minSerialized + 1)) +
+      minSerialized
 
-      await onComplete({
-        data_points: {
-          generatedNumber: String(generatedNumber),
-        },
-      })
-    } catch (err) {
-      if (err instanceof ZodError) {
-        const error = fromZodError(err)
-        await onError({
-          events: [
-            {
-              date: new Date().toISOString(),
-              text: { en: error.name },
-              error: {
-                category: 'WRONG_INPUT',
-                message: `${error.message}`,
-              },
-            },
-          ],
-        })
-        return
-      }
-
-      const error = err as Error
-      await onError({
-        events: [
-          {
-            date: new Date().toISOString(),
-            text: { en: 'Something went wrong while orchestration the action' },
-            error: {
-              category: 'SERVER_ERROR',
-              message: error.message,
-            },
-          },
-        ],
-      })
-    }
+    await onComplete({
+      data_points: {
+        generatedNumber: String(generatedNumber),
+      },
+    })
   },
 }
