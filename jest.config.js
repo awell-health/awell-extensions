cf46ff14d8a664506530b19e2d81793e4a39e1b6
Michal Grzelak <grzelakm@outlook.com>
2023-08-17 11:35:29 +0200

    feat(): update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 // Make sure jest runs with UTC TZ (to avoid failed tests locally)
 process.env.TZ = 'UTC'
-
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  modulePathIgnorePatterns: [
+    '<rootDir>/dist/',
+    '<rootDir>/.vscode/',
+    '<rootDir>/.yalc/',
+  ],
 }
