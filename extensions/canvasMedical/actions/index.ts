60c0f4b5b6250b245fbf8621b12a4875ab4d2f88
kubiak85 <mateusz.kubiak@htdhealth.com>
2023-08-25 15:55:40 +0200

    feat() remove imports

--- extensions/canvasMedical/actions/index.ts
+++ extensions/canvasMedical/actions/index.ts
@@ -1,22 +1,16 @@
-import { getPatient } from './getPatient'
 import { updatePatient } from './updatePatient'
 import { createPatient } from './createPatient'
-import { getAppointment } from './getAppointment'
 import { updateAppointment } from './updateAppointment'
 import { createAppointment } from './createAppointment'
-import { getTask } from './getTask'
 import { updateTask } from './updateTask'
 import { createTask } from './createTask'
 import { extractPatientInfo } from './extractPatientFields'
 
 export const actions = {
-  getPatient,
   updatePatient,
   createPatient,
-  getAppointment,
   updateAppointment,
   createAppointment,
-  getTask,
   updateTask,
   createTask,
   extractPatientInfo,
