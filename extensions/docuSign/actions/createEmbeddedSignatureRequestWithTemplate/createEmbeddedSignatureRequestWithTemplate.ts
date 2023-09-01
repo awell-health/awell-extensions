6e3f3a580d4e652e849f09eb60d3304c09445e23
Michal Grzelak <grzelakm@outlook.com>
2023-09-01 13:49:09 +0200

    feat(): fix test

--- extensions/docuSign/actions/createEmbeddedSignatureRequestWithTemplate/createEmbeddedSignatureRequestWithTemplate.ts
+++ extensions/docuSign/actions/createEmbeddedSignatureRequestWithTemplate/createEmbeddedSignatureRequestWithTemplate.ts
@@ -80,7 +80,7 @@ export const createEmbeddedSignatureRequestWithTemplate: Action<
         userName: signerName,
         clientUserId: patientId,
         returnUrl: replaceStringVariables(returnUrlTemplate, {
-          sessionId,
+          sessionId: sessionId ?? '',
           stakeholderId: patientId,
           pathwayId,
           activityId,
