70759da88a90fb4d2591b0d453d19806b60a4080
Michal Grzelak <michal.grzelak@htdhealth.com>
2023-09-06 11:20:03 +0200

    [Healthie] Map gender to sex in iso format (#241)
    * feat(): add gender to sex in iso format mapper
    
    * chore(): add changelog
    
    * chore(): bump version
    
    * feat(): fix tests
    
    * feat(): rename sexIso to isoSex

--- extensions/healthie/actions/getPatient.ts
+++ extensions/healthie/actions/getPatient.ts
@@ -8,6 +8,7 @@ import { Category } from '@awell-health/extensions-core'
 import { HealthieError, mapHealthieToActivityError } from '../errors'
 import { getSdk } from '../gql/sdk'
 import { initialiseClient } from '../graphqlClient'
+import { mapHealthieGenderToIsoSex } from '../helpers'
 import { type settings } from '../settings'
 import { validateGetPatient } from '../validation/getPatient.zod'
 
@@ -37,6 +38,10 @@ const dataPoints = {
   gender: {
     key: 'gender',
     valueType: 'string',
+  },
+  isoSex: {
+    key: 'isoSex',
+    valueType: 'number',
   },
   email: {
     key: 'email',
@@ -89,6 +94,7 @@ export const getPatient: Action<
             dob,
             email: data.user?.email,
             gender: data.user?.gender,
+            isoSex: String(mapHealthieGenderToIsoSex(data.user?.gender)),
             phoneNumber,
             groupName: data.user?.user_group?.name,
             primaryProviderId: data.user?.dietitian_id,
