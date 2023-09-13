266e9aea82a05cbf08a7f61f4c72437b26473135
Ashvin Pradhan-Shenoy <ashvin.ps@gmail.com>
2023-09-13 12:39:07 -0700

    feat(wellinks-extension): adds test for missing Flourish Settings

--- extensions/wellinks/actions/checkFlourishCustomer/checkFlourishCustomer.test.ts
+++ extensions/wellinks/actions/checkFlourishCustomer/checkFlourishCustomer.test.ts
@@ -133,4 +133,41 @@ describe('Check Flourish Customer', () => {
     })
 >>>>>>> fcb7efc (feat(wellinks-extension): Adds a new action to check if a Flourish Customer exists)
   })
+
+  test('should call onError when the Flourish Settings are not set', async () => {
+    const validPayload = generateTestPayload({
+      fields: {
+        identifier: 'identifier',
+      },
+      settings: {
+        ...mockSettings,
+        flourishApiKey: undefined,
+        flourishApiUrl: undefined,
+        flourishClientExtId: undefined,
+      },
+    })
+    WellinksFlourishClientMockImplementation.user.exists.mockImplementationOnce(
+      () => {
+        return true
+      }
+    )
+    await checkFlourishCustomer.onActivityCreated(
+      validPayload,
+      onComplete,
+      onError
+    )
+
+    expect(onComplete).not.toBeCalled()
+    expect(onError).toHaveBeenNthCalledWith(1, {
+      events: expect.arrayContaining([
+        expect.objectContaining({
+          error: {
+            category: 'SERVER_ERROR',
+            message:
+              'Validation error: Required at "settings.flourishApiKey"; Required at "settings.flourishApiUrl"; Required at "settings.flourishClientExtId"',
+          },
+        }),
+      ]),
+    })
+  })
 })
