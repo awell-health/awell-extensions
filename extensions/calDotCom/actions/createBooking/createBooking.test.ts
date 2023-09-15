7fc4aa711322ee673e10797d4f15904347b784f2
Michal Grzelak <grzelakm@outlook.com>
2023-09-15 09:04:17 +0200

    Add create booking boilerplate

---
+++ extensions/calDotCom/actions/createBooking/createBooking.test.ts
@@ -0,0 +1,49 @@
+import { createBooking } from './createBooking'
+import { generateTestPayload } from '../../../../src/tests'
+import { mockReturnValue, sampleBooking } from '../../__mocks__/calComApi'
+
+jest.mock('../../calComApi', () => jest.fn(() => mockReturnValue))
+
+describe('Create booking', () => {
+  const onComplete = jest.fn()
+  const onError = jest.fn()
+
+  const basePayload = generateTestPayload({
+    fields: {
+      bookingId: String(sampleBooking.id),
+      title: sampleBooking.title,
+      description: undefined,
+      status: sampleBooking.status,
+      start: sampleBooking.startTime,
+      end: sampleBooking.endTime,
+    },
+    settings: {
+      apiKey: 'abc123',
+    },
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('Should call the onComplete callback', async () => {
+    await createBooking.onActivityCreated(basePayload, onComplete, onError)
+
+    expect(mockReturnValue.updateBooking).toHaveBeenCalledWith(
+      String(sampleBooking.id),
+      {
+        title: sampleBooking.title,
+        description: undefined,
+        status: sampleBooking.status,
+        start: sampleBooking.startTime,
+        end: sampleBooking.endTime,
+      }
+    )
+    expect(onComplete).toHaveBeenCalledWith({
+      data_points: {
+        bookingId: String(sampleBooking.id),
+        bookingUid: sampleBooking.uid,
+      },
+    })
+  })
+})
---
+++ extensions/calDotCom/actions/createBooking/index.ts
@@ -0,0 +1,1 @@
+export { createBooking } from './createBooking'
