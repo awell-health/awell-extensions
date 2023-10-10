64c6fb68f0fe21208874aa377a4a110b010b5850
Michal Grzelak <grzelakm@outlook.com>
2023-10-10 12:30:31 +0200

    feat(): add icon url

--- extensions/iterable/index.ts
+++ extensions/iterable/index.ts
@@ -6,8 +6,8 @@ import { settings } from './settings'
 export const Iterable: Extension = {
   key: 'iterable',
   title: 'Iterable',
-  // TODO: needs icon url
-  icon_url: '',
+  icon_url:
+    'https://res.cloudinary.com/da7x4rzl4/image/upload/v1696933028/Awell%20Extensions/logo_1668536435066.jpg',
   description: 'Iterable is a marketing automation platform.',
   category: Category.COMMUNICATION,
   author: {
