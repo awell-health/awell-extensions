05af3e839600f09f967cb4445cfb42375fb8ed74
JB <jonathan@awellhealth.com>
2023-10-10 17:56:24 -0700

    feat(test-server): added a test server to support testing an extension action's input/output

---
+++ src/test-server.ts
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import express from 'express'
+import bodyParser from 'body-parser'
+import type Types from '@awell-health/extensions-core'
+import { extensions } from '../extensions'
+
+const app = express()
+const port = 3000
+
+type QueueInput = (
+  | Parameters<Types.OnCompleteCallback>[0]
+  | Parameters<Types.OnErrorCallback>[0]
+) & { response: 'success' | 'failure' }
+
+const queue: QueueInput[] = []
+
+app.use(bodyParser.json())
+
+app.get('/:extension', async (req, res) => {
+  const extensionKey = req.params.extension
+  const extension = extensions.find(({ key }) => key === extensionKey)
+  if (extension === undefined) {
+    res.status(404).send(`Extension ${extensionKey} not found`)
+    return
+  }
+  res.send(extension)
+})
+
+app.post('/:extension/:action', async (req, res) => {
+  const extensionKey = req.params.extension
+  const actionKey = req.params.action
+  const extension = extensions.find(({ key }) => key === extensionKey)
+  if (extension === undefined) {
+    res.status(404).send(`Extension ${extensionKey} not found`)
+    return
+  }
+  const action = Object.values(extension.actions).find(
+    ({ key }) => key === actionKey
+  )
+  if (action === undefined) {
+    res
+      .status(404)
+      .send(`Action ${actionKey} not found in extension ${extensionKey}`)
+    return
+  }
+  const payload = req.body as Types.NewActivityPayload
+  const onCompleteCb = createOnCompleteCallback(payload)
+  const onErrorCb = createOnErrorCallback(payload)
+  await action.onActivityCreated(payload, onCompleteCb, onErrorCb)
+  const result = queue.shift()
+  res.send(result)
+})
+
+app.listen(port, () => {
+  console.log(`Test server listening at http://localhost:${port}`)
+})
+
+const createOnCompleteCallback = (
+  payload: Types.NewActivityPayload
+): Types.OnCompleteCallback => {
+  return async (params = {}) => {
+    queue.push({ ...params, response: 'success' })
+    console.log({ ...params, response: 'success' })
+  }
+}
+const createOnErrorCallback = (
+  payload: Types.NewActivityPayload
+): Types.OnErrorCallback => {
+  return async (params = {}) => {
+    queue.push({ ...params, response: 'failure' })
+    console.error({ ...params, response: 'failure' })
+  }
+}
