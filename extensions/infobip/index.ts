09d259d83ba13fe386be7d62e49590826480c8e4
Michal Grzelak <grzelakm@outlook.com>
2023-10-17 10:24:56 +0200

    feat(): add icon url

--- extensions/infobip/index.ts
+++ extensions/infobip/index.ts
@@ -6,8 +6,8 @@ import { settings } from './settings'
 export const Infobip: Extension = {
   key: 'infobip',
   title: 'Infobip',
-  // TODO: add url
-  icon_url: '',
+  icon_url:
+    'https://res.cloudinary.com/da7x4rzl4/image/upload/v1697459181/Awell%20Extensions/download_3.png',
   description:
     'Infobip is a global cloud communications platform that provides a wide range of communication and customer engagement solutions for businesses.',
   category: Category.COMMUNICATION,
