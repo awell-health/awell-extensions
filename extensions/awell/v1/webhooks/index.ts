58f968e62b7809451f640dae71542cf490e02658
Rahul Keerthi <rahulkeerthi2@gmail.com>
2023-10-26 23:19:22 +0100

    fix(awell): fix webhook name in index

--- extensions/awell/v1/webhooks/index.ts
+++ extensions/awell/v1/webhooks/index.ts
@@ -1,8 +1,8 @@
 import { pathwayCompleted } from './pathwayCompleted'
-import { startPathway } from './pathwayStart'
+import { pathwayStart } from './pathwayStart'
 
 // We have to export this type for the declaration file, so the compiler can write an object type literal
 // https://github.com/microsoft/TypeScript/issues/5711
 export type { PathwayCompletedPayload } from './pathwayCompleted'
-export type { StartPathwayPayload } from './pathwayStart'
-export const webhooks = [pathwayCompleted, startPathway]
+export type { PathwayStartPayload } from './pathwayStart'
+export const webhooks = [pathwayCompleted, pathwayStart]
