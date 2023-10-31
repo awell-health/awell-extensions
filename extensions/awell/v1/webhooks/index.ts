f2757da467d5a5c6670a579db65c21b957b604a9
Rahul Keerthi <rahulkeerthi2@gmail.com>
2023-10-31 12:32:46 +0000

    webhook(awell): add startPathway webhook (#289)
    * webhook(awell): add startPathway webhook

--- extensions/awell/v1/webhooks/index.ts
+++ extensions/awell/v1/webhooks/index.ts
@@ -1,6 +1,8 @@
 import { pathwayCompleted } from './pathwayCompleted'
+import { pathwayStart } from './pathwayStart'
 
 // We have to export this type for the declaration file, so the compiler can write an object type literal
 // https://github.com/microsoft/TypeScript/issues/5711
 export type { PathwayCompletedPayload } from './pathwayCompleted'
-export const webhooks = [pathwayCompleted]
+export type { PathwayStartPayload } from './pathwayStart'
+export const webhooks = [pathwayCompleted, pathwayStart]
