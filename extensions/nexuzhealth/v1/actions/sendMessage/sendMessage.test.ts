d9a10636727afa01e478906b1885d24d6892e900
Nick Hellemans <nick@awellhealth.com>
2023-12-11 14:32:10 +0100

    nexuzhealth demo extension (#301)
    * feat(nexuzhealth): demo extension
    
    ---------
    
    Co-authored-by: Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>

---
+++ extensions/nexuzhealth/v1/actions/sendMessage/sendMessage.test.ts
@@ -0,0 +1,26 @@
+import { sendMessage } from '.'
+import { generateTestPayload } from '../../../../../src/tests'
+
+describe('Send message', () => {
+  const onComplete = jest.fn()
+  const onError = jest.fn()
+
+  const basePayload = generateTestPayload({
+    fields: {
+      subject: 'test-subject',
+      body: 'test-body',
+    },
+    settings: {},
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('Should call the onComplete callback', async () => {
+    await sendMessage.onActivityCreated(basePayload, onComplete, onError)
+
+    expect(onComplete).toHaveBeenCalled()
+    expect(onError).not.toHaveBeenCalled()
+  })
+})
---
+++ extensions/nexuzhealth/v1/actions/sendQuestionnaire/sendQuestionnaire.test.ts
@@ -0,0 +1,25 @@
+import { sendQuestionnaire } from '.'
+import { generateTestPayload } from '../../../../../src/tests'
+
+describe('Send questionnaire', () => {
+  const onComplete = jest.fn()
+  const onError = jest.fn()
+
+  const basePayload = generateTestPayload({
+    fields: {
+      questionnaireId: 'test-questionnaire',
+    },
+    settings: {},
+  })
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('Should call the onComplete callback', async () => {
+    await sendQuestionnaire.onActivityCreated(basePayload, onComplete, onError)
+
+    expect(onComplete).toHaveBeenCalled()
+    expect(onError).not.toHaveBeenCalled()
+  })
+})
