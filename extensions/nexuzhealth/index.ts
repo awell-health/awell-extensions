a43194b69ffb3dabd9ad72788e02602e64384845
Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
2023-12-14 18:04:55 +0100

    chore(extensions): mark nexuzhealth as demo extension

--- extensions/nexuzhealth/index.ts
+++ extensions/nexuzhealth/index.ts
@@ -10,7 +10,7 @@ export const nexuzhealth: Extension = {
     'Nexuzhealth develops KWS (Klinisch Werkstation), an EHR for hospitals, healthcare institutions, home nursing, and general practitioners.',
   icon_url:
     'https://res.cloudinary.com/da7x4rzl4/image/upload/v1702299062/Awell%20Extensions/nexuzhealth.png',
-  category: Category.EHR_INTEGRATIONS,
+  category: Category.DEMO,
   author: {
     authorType: AuthorType.AWELL,
   },
