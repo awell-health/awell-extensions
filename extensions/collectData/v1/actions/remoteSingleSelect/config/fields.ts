d81eec77721f18a0e35c58e18007746b6498c38b
Rahul Keerthi <rahul@awellhealth.com>
2024-01-12 14:40:11 +0000

    refactor(remoteSingleSelect): update fields (#309)

--- extensions/collectData/v1/actions/remoteSingleSelect/config/fields.ts
+++ extensions/collectData/v1/actions/remoteSingleSelect/config/fields.ts
@@ -9,23 +9,30 @@ import { JsonStringValidationSchema } from '../../../../../sendbird/v1/validatio
 export const fields = {
   label: {
     id: 'label',
-    label: 'Label text',
+    label: 'Select - Label',
     description:
       'Enter the label text that will be displayed before the selector.',
     type: FieldType.STRING,
     required: true,
   },
+  mandatory: {
+    id: 'mandatory',
+    label: 'Select - Response required?',
+    description: 'The user must select an option before continuing.',
+    type: FieldType.BOOLEAN,
+    required: true,
+  },
   url: {
     id: 'url',
-    label: 'Options endpoint URL',
+    label: 'Options - Endpoint URL',
     description:
-      'Enter the URL where the options to display in the selector can be fetched. The endpoint must return an array of objects with the following properties: `label` and `value`.',
+      'Enter the URL (starting with `https://`) where the options to display can be fetched. The endpoint must return an array of objects with the following properties: `id`, `label` and `value`.',
     type: FieldType.STRING,
     required: true,
   },
   headers: {
     id: 'headers',
-    label: 'Options endpoint headers',
+    label: 'Options - Headers',
     description:
       'Enter the headers to send to the options source URL. The headers must be in JSON format.',
     type: FieldType.JSON,
@@ -33,18 +40,11 @@ export const fields = {
   },
   queryParam: {
     id: 'queryParam',
-    label: 'Options endpoint search query param key',
+    label: 'Options - Search query param',
     description:
       'Enter the key of the free text search query param if the endpoint supports this e.g. `search` for `https://example.com/options?search=foo`.',
     type: FieldType.STRING,
     required: false,
-  },
-  mandatory: {
-    id: 'mandatory',
-    label: 'Is response required?',
-    description: 'The user must select an option before continuing.',
-    type: FieldType.BOOLEAN,
-    required: true,
   },
 } satisfies Record<string, Field>
 
