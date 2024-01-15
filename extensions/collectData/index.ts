eea723638f00521313484f9ed0f7f50eaba6e8b1
Nick Hellemans <nick@awellhealth.com>
2024-01-15 15:57:53 +0100

    Advanced data collection extension (#310)
    * chore(): copy feedback
    
    * chore(): update description
    
    * docs(collectData): update README with search logic
    
    ---------
    
    Co-authored-by: Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
    Co-authored-by: Rahul Keerthi <rahul@awellhealth.com>

--- extensions/collectData/index.ts
+++ extensions/collectData/index.ts
@@ -1,23 +1,21 @@
 import { type Extension } from '@awell-health/extensions-core'
 import { AuthorType, Category } from '@awell-health/extensions-core'
-import {
-  remoteSingleSelect
-} from './v1/actions'
+import { remoteSingleSelect } from './v1/actions'
 import { settings } from './settings'
 
 export const CollectData: Extension = {
   key: 'collectData',
-  title: 'Collect Data',
+  title: 'Advanced data collection',
   icon_url:
     'https://res.cloudinary.com/da7x4rzl4/image/upload/v1678870116/Awell%20Extensions/Awell_Logo.png',
   description:
-    'Collect data from users to ingest into care flows',
+    'This extension allows you to collect data from your users using a variety of more advanced input types and data sources.',
   category: Category.FORMS,
   author: {
     authorType: AuthorType.AWELL,
   },
   actions: {
-    remoteSingleSelect
+    remoteSingleSelect,
   },
   settings,
 }
