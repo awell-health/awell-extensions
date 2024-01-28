143a4099e1bddb69ddf5db3f5be5bc7685257b13
Danijel Bjelajac <danijel@awellhealth.com>
2024-01-28 12:09:44 +0100

    refactor(healthie): update fields description

--- extensions/healthie/actions/checkPatientTag/config/fields.ts
+++ extensions/healthie/actions/checkPatientTag/config/fields.ts
@@ -4,15 +4,14 @@ export const fields = {
   id: {
     id: 'id',
     label: 'Tag ID',
-    description:
-      'The ID of the tag in Healthie you would like to check in patient active tags.',
+    description: 'The ID of the tag in Healthie.',
     type: FieldType.STRING,
   },
   patientId: {
     id: 'patientId',
     label: 'Patient ID',
     description:
-      'The ID of the patient in Healthie you would like to retrieve.',
+      'The ID of the patient in Healthie for whom you would like to check is tag active.',
     type: FieldType.STRING,
   },
 } satisfies Record<string, Field>
