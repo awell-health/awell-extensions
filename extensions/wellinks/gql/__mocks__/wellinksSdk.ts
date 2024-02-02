5501786bb39ff7edbb514c5843a501b14fa191db
JaphetMataImp <144716242+JaphetMataImp@users.noreply.github.com>
2024-02-01 19:08:36 -0600

    WL-3494 Fill custom Healthie Form based on a JSON (#326)
    * feat(wellinks-extension): Updates Flourish Client Content Type (#313)
    
    ---------
    Co-authored-by: Ashvin Pradhan-Shenoy <ashvin.ps@gmail.com>

--- extensions/wellinks/gql/__mocks__/wellinksSdk.ts
+++ extensions/wellinks/gql/__mocks__/wellinksSdk.ts
@@ -1,22 +1,41 @@
 export const mockGetSdkReturn = {
     getChartingItems: jest.fn((args) => {
-        return { data: { 
-            // chartingItems: null
-         }}
-    }),
-    getScheduledAppointments: jest.fn((args) => {
-        return { 
+        return {
             data: {
-
+                // chartingItems: null
             }
         }
     }),
+    getScheduledAppointments: jest.fn((args) => {
+        return {
+            data: {}
+        }
+    }),
     getConversationMemberships: jest.fn((args) => {
-        return { 
+        return {
+            data: {}
+        }
+    }),
+    getFormTemplate: jest.fn((args) => {
+        return {
             data: {
-
-        }}
-    })
+                customModuleForm: {
+                    use_for_charting: true,
+                    custom_modules: [
+                        {
+                            id: 'question-1',
+                            mod_type: 'textarea',
+                        },
+                    ],
+                },
+            },
+        }
+    }),
+    createFormAnswerGroup: jest.fn((args) => {
+        return {
+            data: {},
+        }
+    }),
 }
 
 export const mockGetSdk = (params: any): any => {
