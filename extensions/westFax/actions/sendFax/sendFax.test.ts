57fde81635ddfcd1430f6227f6cf016920e7d765
Flávio Ferreira <flavio@awellhealth.com>
2024-02-06 13:00:31 +0000

    WestFax Extensions - fix result handling (#337)
    * WestFax Extensions - fix result handling
    
    * Add small comment
    
    * fix typo

---
+++ extensions/westFax/actions/sendFax/sendFax.test.ts
@@ -0,0 +1,34 @@
+import { generateTestPayload } from '../../../../src/tests'
+import { sendFax } from './sendFax'
+
+describe('send fax action', () => {
+  const onComplete = jest.fn()
+  const onError = jest.fn()
+
+  test('Should not send a fax', async () => {
+    await sendFax.onActivityCreated(
+      generateTestPayload({
+        fields: {
+          productId: 'wrongproductid',
+          number: '234234234234',
+          content: `
+          <h1 class="slate-h1">Create some nice Fax</h1><p class="slate-p">This is a simple fax to test the integration.</p><p class="slate-p"></p>
+          `,
+          feedbackEmail: undefined,
+        },
+        settings: {
+          username: 'asd',
+          password: 'somewrongpass',
+        },
+      }),
+      onComplete,
+      onError
+    )
+
+    expect(onComplete).not.toHaveBeenCalled()
+    expect(onError).toHaveBeenCalled()
+  })
+
+  
+
+})
