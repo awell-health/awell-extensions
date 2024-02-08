524fff61ba524551876421038c424a443b1d9733
Japhet <144716242+JaphetMataImp@users.noreply.github.com>
2024-02-08 13:18:55 -0600

    Users/japhet/wl 3494 fix action title (#338)
    Changes:
    Title: from Fill Form entrance to Create charting note - advanced
    
    Add Validation for the FormAnswers:
    user_id must be equal to healthie_patient_id
    
    using Zod.superRefine function
    
    Add test for Validation: extensions/wellinks/actions/createChartingNoteAdvanced/createChartingNoteAdvanced-fail.test.ts
    
    ---------
    
    Co-authored-by: Ashvin Pradhan-Shenoy <ashvin.ps@gmail.com>

--- extensions/wellinks/actions/createChartingNoteAdvanced/config/fields.ts
+++ extensions/wellinks/actions/createChartingNoteAdvanced/config/fields.ts
@@ -3,49 +3,49 @@ import { z } from 'zod';
 
 export const fields = {
   healthie_patient_id: {
-      id: 'healthie_patient_id',
-      label: 'Healthie Patient ID',
-      description:
-          'The ID of the patient you would like to create a charting note for.',
-      type: FieldType.STRING,
-      required: true,
+    id: 'healthie_patient_id',
+    label: 'Healthie Patient ID',
+    description:
+      'The ID of the patient you would like to create a charting note for.',
+    type: FieldType.STRING,
+    required: true,
   },
   form_id: {
-      id: 'form_id',
-      label: 'Form ID',
-      description:
-          'The ID of the form you would like to create the charting note against.',
-      type: FieldType.STRING,
-      required: true,
+    id: 'form_id',
+    label: 'Form ID',
+    description:
+      'The ID of the form you would like to create the charting note against.',
+    type: FieldType.STRING,
+    required: true,
   },
   note_content: {
-      id: 'note_content',
-      label: 'Note content',
-      description: 'The json content of the charting note.',
-      type: FieldType.JSON,
-      required: true,
+    id: 'note_content',
+    label: 'Note content',
+    description: 'The json content of the charting note.',
+    type: FieldType.JSON,
+    required: true,
   },
   marked_locked: {
-      id: 'marked_locked',
-      label: 'Mark as locked?',
-      description: 'This field allows you to mark the content as locked.',
-      type: FieldType.BOOLEAN,
-      required: false,
+    id: 'marked_locked',
+    label: 'Mark as locked?',
+    description: 'This field allows you to mark the content as locked.',
+    type: FieldType.BOOLEAN,
+    required: false,
   },
   appointment_id: {
-      id: 'appointment_id',
-      label: 'Appointment ID',
-      description:
-          'The ID of the appointment you would like the charting note associated with.',
-      type: FieldType.STRING,
-      required: false,
+    id: 'appointment_id',
+    label: 'Appointment ID',
+    description:
+      'The ID of the appointment you would like the charting note associated with.',
+    type: FieldType.STRING,
+    required: false,
   },
 } satisfies Record<string, Field>
 
 const FormAnswerSchema = z.object({
-    custom_module_id: z.string(),
-    user_id: z.string(),
-    answer: z.string(),
+  custom_module_id: z.string(),
+  user_id: z.string(),
+  answer: z.string(),
 });
 
 const FormAnswersSchema = z.array(FormAnswerSchema);
@@ -59,4 +59,15 @@ export const FieldsValidationSchema = z.object({
     .transform((v) => FormAnswersSchema.parse(v)),
   marked_locked: z.boolean().optional(),
   appointment_id: z.string().optional(),
+}).superRefine((data, ctx) => {
+  for (const answer of data.note_content) {
+    if (answer.user_id !== data.healthie_patient_id) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `User ID ${answer.user_id} does not match healthie_patient_id ${data.healthie_patient_id}`,
+        path: ['note_content'],
+        fatal: true
+      });
+    }
+  }
 });
