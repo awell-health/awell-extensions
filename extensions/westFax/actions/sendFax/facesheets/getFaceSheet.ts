6b2fe6980b669e044cf3ae38127e2b5cd5f83e1d
Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
2024-02-12 13:44:38 +0100

    feat(westfax): remote fetch the facesheets

--- extensions/westFax/actions/sendFax/facesheets/getFaceSheet.ts
+++ extensions/westFax/actions/sendFax/facesheets/getFaceSheet.ts
@@ -1,15 +1,30 @@
-import fs from 'fs'
-import { promisify } from 'util'
+import axios from 'axios'
+import { isEmpty, isNil } from 'lodash'
 
-const readFileAsync = promisify(fs.readFile)
+const DEFAULT_FACESHEET =
+  'https://res.cloudinary.com/da7x4rzl4/image/upload/v1707740015/Awell%20Extensions/WestFax/default_facesheet.pdf'
 
-export const getFaceSheet = async (): Promise<Buffer> => {
+export const getFaceSheet = async (
+  customFaceSheetUrl?: string
+): Promise<Buffer> => {
   try {
-    const faceSheetPdfBuffer = await readFileAsync(
-      './extensions/westFax/actions/sendFax/facesheets/templates/default_facesheet.pdf'
-    )
+    const pdfUrl =
+      isNil(customFaceSheetUrl) || isEmpty(customFaceSheetUrl)
+        ? DEFAULT_FACESHEET
+        : customFaceSheetUrl
 
-    return faceSheetPdfBuffer
+    const headResponse = await axios.head(pdfUrl)
+
+    // Check if the Content-Type header indicates a PDF file
+    if (headResponse.headers['content-type'] === 'application/pdf') {
+      // If it's a PDF, make a GET request to download the file
+      const response = await axios.get(pdfUrl, { responseType: 'arraybuffer' })
+      const faceSheetPdfBuffer = Buffer.from(response.data)
+
+      return faceSheetPdfBuffer
+    } else {
+      throw new Error('Face sheet URL does not point to a valid PDF file.')
+    }
   } catch (error) {
     console.error('Error reading the face sheet template:', error)
     throw error
