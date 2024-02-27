41d69f1324f449fd716a7acd1e83bbbc6a5b85f3
Namdar Mesri <namdar@awellhealth.com>
2024-02-26 23:04:26 -0800

    ICD-10: update description and title (#349)

--- extensions/icd/index.ts
+++ extensions/icd/index.ts
@@ -4,9 +4,9 @@ import actions from './actions'
 
 export const icd: Extension = {
   key: 'icd',
-  title: 'An ICD extension!',
+  title: 'ICD10 Lookup',
   description:
-    'An example extension developers can look at to get started with building their first extension.',
+    'This extension allows you to get the name of the diseases based on its ICD.',
   icon_url:
     'https://res.cloudinary.com/da7x4rzl4/image/upload/v1678870116/Awell%20Extensions/Awell_Logo.png',
   category: Category.DATA,
