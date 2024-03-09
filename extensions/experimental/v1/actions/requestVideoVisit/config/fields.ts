e181c4963093c83eb49535f3d8128f9a8013ec11
Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
2024-03-09 07:49:10 +0100

    feat(experiment): enter medication data

--- extensions/experimental/v1/actions/requestVideoVisit/config/fields.ts
+++ extensions/experimental/v1/actions/requestVideoVisit/config/fields.ts
@@ -1,8 +1,16 @@
-import { type Field } from '@awell-health/extensions-core'
+import { FieldType, type Field } from '@awell-health/extensions-core'
 import { z, type ZodTypeAny } from 'zod'
 
-export const fields = {} satisfies Record<string, Field>
+export const fields = {
+  deepLink: {
+    id: 'deepLink',
+    label: 'Deep link',
+    description: 'Insert a deep link for testing',
+    type: FieldType.STRING,
+    required: false,
+  },
+} satisfies Record<string, Field>
 
-export const FieldsValidationSchema = z.object(
-  {} satisfies Record<keyof typeof fields, ZodTypeAny>
-)
+export const FieldsValidationSchema = z.object({
+  deepLink: z.string().optional(),
+} satisfies Record<keyof typeof fields, ZodTypeAny>)
---
+++ extensions/experimental/v1/actions/requestVideoVisit/requestVideoVisit.test.ts
@@ -0,0 +1,46 @@
+import { requestVideoVisit } from '.'
+import { generateTestPayload } from '../../../../../src/tests'
+
+describe('Experimental - Request video visit', () => {
+  const onComplete = jest.fn()
+  const onError = jest.fn()
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('Should work', async () => {
+    const mockOnActivityCreateParams = generateTestPayload({
+      fields: {
+        deepLink: 'https://www.google.be',
+      },
+      settings: {},
+    })
+
+    await requestVideoVisit.onActivityCreated(
+      mockOnActivityCreateParams,
+      onComplete,
+      onError
+    )
+
+    expect(onError).not.toBeCalled()
+  })
+
+  test('Should not work', async () => {
+    const mockOnActivityCreateParams = generateTestPayload({
+      fields: {
+        deepLink: true,
+      },
+      settings: {},
+    })
+
+    await requestVideoVisit.onActivityCreated(
+      // @ts-expect-error zod will catch this
+      mockOnActivityCreateParams,
+      onComplete,
+      onError
+    )
+
+    expect(onError).toBeCalled()
+  })
+})
