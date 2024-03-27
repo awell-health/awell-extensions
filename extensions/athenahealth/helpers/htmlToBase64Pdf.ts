991ff7583ac4674eabe38032b52361e3008a1be6
JB <jonathan@awellhealth.com>
2024-03-27 14:17:03 -0700

    fix(athenahealth): removed puppeteer, fixed test

--- extensions/athenahealth/helpers/htmlToBase64Pdf.ts
+++ extensions/athenahealth/helpers/htmlToBase64Pdf.ts
@@ -1,6 +1,6 @@
 export const htmlToBase64Pdf = async (html: string): Promise<string> => {
   const base64Html = Buffer.from(html).toString('base64')
-
+  const controller = new AbortController()
   const timeoutinMS = async (ms: number): Promise<never> => {
     // eslint-disable-next-line promise/param-names
     return await new Promise((_, reject) => {
@@ -14,12 +14,18 @@ export const htmlToBase64Pdf = async (html: string): Promise<string> => {
     headers: {
       'Content-Type': 'application/json',
     },
-
+    signal: controller.signal,
     body: JSON.stringify({
       html: base64Html,
     }),
   })
-  const response = await Promise.race([request, timeoutinMS(30000)])
+  const response = await Promise.race([request, timeoutinMS(30000)]).catch(
+    (err) => {
+      controller.abort()
+      console.error(err)
+      throw err
+    }
+  )
   const { pdf: base64Pdf } = await response.json()
   return base64Pdf
 }
