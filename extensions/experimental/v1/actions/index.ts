c6e05b096076471d24496c393ac7c093635af645
Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
2024-03-29 13:16:45 +0100

    chore(experiment): add new action for testing and local dev

--- extensions/experimental/v1/actions/index.ts
+++ extensions/experimental/v1/actions/index.ts
@@ -1,3 +1,4 @@
 export { requestVideoVisit } from './requestVideoVisit'
 export { enterMedication } from './enterMedication'
 export { wait } from './wait'
+export { patientRecommendation } from './patientRecommendation'
