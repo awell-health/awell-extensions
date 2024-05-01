36378094c12e9d8e8b8c469e4aa5dcc27ab97751
Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
2024-05-01 12:36:10 +0200

    fix(): missing success field

--- extensions/awell/v1/sdk/graphql/addIdentifier.mutation.ts
+++ extensions/awell/v1/sdk/graphql/addIdentifier.mutation.ts
@@ -1,6 +1,8 @@
 export const addIdentifierToPatientMutation = `
 mutation AddIdentifierToPatient($input: AddIdentifierToPatientInput!) {
     addIdentifierToPatient(input: $input) {
+      success
+      code
       patient {
         id
       }
