de7cfb4deab0ecec5b520dbd728da20ab59367f5
Nick Hellemans <nick@awellhealth.com>
2024-05-07 05:08:03 -0700

    chore(medplum): link questionnaireresponse to patient

--- extensions/medplum/actions/submitQuestionnaireResponse/config/fields.ts
+++ extensions/medplum/actions/submitQuestionnaireResponse/config/fields.ts
@@ -1,8 +1,17 @@
-import { type Field } from '@awell-health/extensions-core'
+import { FieldType, type Field } from '@awell-health/extensions-core'
 import { z, type ZodTypeAny } from 'zod'
 
-export const fields = {} satisfies Record<string, Field>
+export const fields = {
+  patientId: {
+    id: 'patientId',
+    label: 'Patient ID',
+    description:
+      'The identifier of the patient in Medplum to assign the response to',
+    type: FieldType.STRING,
+    required: true,
+  },
+} satisfies Record<string, Field>
 
-export const FieldsValidationSchema = z.object(
-  {} satisfies Record<keyof typeof fields, ZodTypeAny>
-)
+export const FieldsValidationSchema = z.object({
+  patientId: z.string().min(1),
+} satisfies Record<keyof typeof fields, ZodTypeAny>)
