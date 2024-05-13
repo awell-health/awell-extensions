1f85f6957a717490d73a8750b2b83908a1a90941
Nick Hellemans <nick@awellhealth.com>
2024-05-12 23:32:53 -0700

    feat(): productize medication action (#386)

--- extensions/collectData/index.ts
+++ extensions/collectData/index.ts
@@ -1,6 +1,6 @@
 import { type Extension } from '@awell-health/extensions-core'
 import { AuthorType, Category } from '@awell-health/extensions-core'
-import { arrayTest, remoteSingleSelect } from './v1/actions'
+import { collectMedication, remoteSingleSelect } from './v1/actions'
 import { settings } from './settings'
 
 export const CollectData: Extension = {
@@ -9,13 +9,13 @@ export const CollectData: Extension = {
   icon_url:
     'https://res.cloudinary.com/da7x4rzl4/image/upload/v1678870116/Awell%20Extensions/Awell_Logo.png',
   description:
-    'This extension allows you to collect data from your users using a variety of more advanced input types and data sources.',
+    'This extension allows you to collect data from your users using a variety of advanced input types and data sources.',
   category: Category.FORMS,
   author: {
     authorType: AuthorType.AWELL,
   },
   actions: {
-    arrayTest,
+    collectMedication,
     remoteSingleSelect,
   },
   settings,
---
+++ extensions/collectData/v1/actions/collectMedication/collectMedication.ts
@@ -0,0 +1,23 @@
+import { type Action } from '@awell-health/extensions-core'
+import { type settings } from '../../../settings'
+import { Category } from '@awell-health/extensions-core'
+import { fields, dataPoints } from './config'
+
+export const collectMedication: Action<typeof fields, typeof settings> = {
+  key: 'collectMedication',
+  category: Category.FORMS,
+  title: 'Collect medication',
+  description: 'Collect medication in a semi-structured way from the user',
+  fields,
+  dataPoints,
+  options: {
+    stakeholders: {
+      label: 'Stakeholder',
+      mode: 'single',
+    },
+  },
+  previewable: false,
+  onActivityCreated: async (payload, onComplete, onError): Promise<void> => {
+    // Completion happens in Hosted Pages
+  },
+}
