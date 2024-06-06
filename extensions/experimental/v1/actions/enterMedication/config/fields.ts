859632c4a73b5f6dfc5c41f6866ae21e6c2212c2
Nick Hellemans <nick@awellhealth.com>
2024-06-06 10:36:01 +0200

    feat(): allow setting label

--- extensions/experimental/v1/actions/enterMedication/config/fields.ts
+++ extensions/experimental/v1/actions/enterMedication/config/fields.ts
@@ -1,8 +1,16 @@
-import { type Field } from '@awell-health/extensions-core'
+import { FieldType, type Field } from '@awell-health/extensions-core'
 import { z, type ZodTypeAny } from 'zod'
 
-export const fields = {} satisfies Record<string, Field>
+export const fields = {
+  label: {
+    id: 'label',
+    label: 'Label',
+    type: FieldType.HTML,
+    required: false,
+    description: 'A label or description that will be shown above the question',
+  },
+} satisfies Record<string, Field>
 
-export const FieldsValidationSchema = z.object(
-  {} satisfies Record<keyof typeof fields, ZodTypeAny>
-)
+export const FieldsValidationSchema = z.object({
+  label: z.string().optional(),
+} satisfies Record<keyof typeof fields, ZodTypeAny>)
