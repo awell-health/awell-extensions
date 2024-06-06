903db742b11222feea127bb8fa0688bcf525bc57
Nick Hellemans <nick@awellhealth.com>
2024-06-06 11:09:29 +0200

    chore(): revert backward compatibility, breaking change is ok

--- extensions/experimental/v1/actions/enterMedication/config/datapoints.ts
+++ extensions/experimental/v1/actions/enterMedication/config/datapoints.ts
@@ -1,11 +1,6 @@
 import { type DataPointDefinition } from '@awell-health/extensions-core'
 
 export const dataPoints = {
-  // This data point should be deprecated but keeping it here for backward compatibility
-  stringifiedMedicationData: {
-    key: 'stringifiedMedicationData',
-    valueType: 'string',
-  },
   medicationData: {
     key: 'medicationData',
     valueType: 'json',
