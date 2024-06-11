f6578727db121960e3bb8059f4251401875c6b44
JB <jonathan@awellhealth.com>
2024-06-11 10:55:06 -0700

    Update to newest extensions-core

--- extensions/healthie/lib/sdk/validatePayloadAndCreateSdk.ts
+++ extensions/healthie/lib/sdk/validatePayloadAndCreateSdk.ts
@@ -1,6 +1,6 @@
-import { validate } from '@awell-health/extensions-core'
+import { type SettingsValues, validate } from '@awell-health/extensions-core'
 import z from 'zod'
-import { settingsValidationSchema } from '../../settings'
+import { type settings, settingsValidationSchema } from '../../settings'
 import { getSdk } from './generated/sdk'
 import { initialiseClient } from './graphqlClient'
 
@@ -11,6 +11,15 @@ type ValidatePayloadAndCreateSdk = <T extends z.ZodTypeAny>(args: {
   sdk: ReturnType<typeof getSdk>
   fields: z.infer<(typeof args)['fieldsSchema']>
   settings: z.infer<typeof settingsValidationSchema>
+}>
+
+type ValidateWebhookPayloadAndCreateSdk = <T extends z.ZodTypeAny>(args: {
+  payloadSchema: T
+  payload: unknown
+  settings: SettingsValues<typeof settings>
+}) => Promise<{
+  sdk: ReturnType<typeof getSdk>
+  validatedPayload: z.infer<(typeof args)['payloadSchema']>
 }>
 
 export const validatePayloadAndCreateSdk: ValidatePayloadAndCreateSdk = async ({
@@ -34,3 +43,24 @@ export const validatePayloadAndCreateSdk: ValidatePayloadAndCreateSdk = async ({
 
   return { settings, fields, sdk }
 }
+
+export const validateWebhookPayloadAndCreateSdk: ValidateWebhookPayloadAndCreateSdk =
+  async ({ payloadSchema, payload, settings }) => {
+    const { payload: validatedPayload } = validate({
+      schema: z.object({
+        payload: payloadSchema,
+      }),
+      payload,
+    })
+
+    const client = initialiseClient(settings)
+
+    if (client === undefined)
+      throw new Error(
+        'There was a problem creating the Healthie GraphQL API Client. Please check your extension settings to validate the API URL and API Key.'
+      )
+
+    const sdk = getSdk(client)
+
+    return { validatedPayload, sdk }
+  }
--- extensions/healthie/webhooks/appointmentUpdated.ts
+++ extensions/healthie/webhooks/appointmentUpdated.ts
@@ -1,37 +1,58 @@
-import { isNil } from 'lodash'
 import {
   type DataPointDefinition,
   type Webhook,
 } from '@awell-health/extensions-core'
 import { type HealthieWebhookPayload } from '../lib/types'
+import z from 'zod'
+import { validateWebhookPayloadAndCreateSdk } from '../lib/sdk/validatePayloadAndCreateSdk'
+import { type settings } from '../../awell/settings'
 
 const dataPoints = {
   updatedAppointmentId: {
     key: 'updatedAppointmentId',
     valueType: 'string',
   },
+  appointment: {
+    key: 'appointment',
+    valueType: 'json',
+  },
 } satisfies Record<string, DataPointDefinition>
+
+const payloadSchema = z
+  .object({
+    resource_id: z.string(),
+  })
+  .transform((d) => {
+    return {
+      updatedAppointmentId: d.resource_id,
+    }
+  })
 
 export const appointmentUpdated: Webhook<
   keyof typeof dataPoints,
-  HealthieWebhookPayload
+  HealthieWebhookPayload,
+  typeof settings
 > = {
   key: 'appointmentUpdated',
   dataPoints,
   onWebhookReceived: async ({ payload, settings }, onSuccess, onError) => {
-    const { resource_id: updatedAppointmentId } = payload
-
-    if (isNil(updatedAppointmentId)) {
-      await onError({
-        // We should automatically send a 400 here, so no need to provide info
-      })
-    } else {
-      await onSuccess({
-        data_points: {
-          updatedAppointmentId,
-        },
-      })
-    }
+    const {
+      validatedPayload: { updatedAppointmentId },
+      sdk,
+    } = await validateWebhookPayloadAndCreateSdk({
+      payload,
+      payloadSchema,
+      settings,
+    })
+    const response = await sdk.getAppointment({
+      id: updatedAppointmentId,
+    })
+    await onSuccess({
+      data_points: {
+        updatedAppointmentId,
+        appointment: JSON.stringify(response?.data?.appointment),
+      },
+    })
   },
 }
 
