52eb3716721a1c91e65163ef82f1293843e39482
Abigaila Ekiert <arekiert@gmail.com>
2024-07-03 13:31:15 +0100

    Update requestFormCompletion update and create

--- extensions/healthie/webhooks/requestedFormCompletionCreated.ts
+++ extensions/healthie/webhooks/requestedFormCompletionCreated.ts
@@ -3,7 +3,20 @@ import {
   type DataPointDefinition,
   type Webhook,
 } from '@awell-health/extensions-core'
-import { type HealthieWebhookPayload } from '../lib/types'
+import { HEALTHIE_IDENTIFIER, type HealthieWebhookPayload } from '../lib/types'
+import z from 'zod'
+import { validateWebhookPayloadAndCreateSdk } from '../lib/sdk/validatePayloadAndCreateSdk'
+import { type settings } from '../settings'
+
+const payloadSchema = z
+  .object({
+    resource_id: z.string(),
+  })
+  .transform((data) => {
+    return {
+      createdFormCompletionId: data.resource_id,
+    }
+  })
 
 const dataPoints = {
   createdFormCompletionId: {
@@ -14,24 +27,33 @@ const dataPoints = {
 
 export const requestFormCompletionCreated: Webhook<
   keyof typeof dataPoints,
-  HealthieWebhookPayload
+  HealthieWebhookPayload,
+  typeof settings
 > = {
   key: 'requestFormCompletionCreated',
   dataPoints,
   onWebhookReceived: async ({ payload, settings }, onSuccess, onError) => {
-    const { resource_id: createdFormCompletionId } = payload
-
-    if (isNil(createdFormCompletionId)) {
-      await onError({
-        // We should automatically send a 400 here, so no need to provide info
-      })
-    } else {
-      await onSuccess({
-        data_points: {
-          createdFormCompletionId,
+    const {
+      validatedPayload: { createdFormCompletionId },
+      sdk,
+    } = await validateWebhookPayloadAndCreateSdk({
+      payloadSchema,
+      payload,
+      settings,
+    })
+    const response = await sdk.GetRequestedFormCompletion({ id: createdFormCompletionId })
+    const healthiePatientId = response?.data?.requestedFormCompletion?.recipient_id
+    await onSuccess({
+      data_points: {
+        createdFormCompletionId,
+      },
+      ...(!isNil(healthiePatientId) && {
+        patient_identifier: {
+          system: HEALTHIE_IDENTIFIER,
+          value: healthiePatientId,
         },
-      })
-    }
+      }),
+    })
   },
 }
 
--- extensions/healthie/webhooks/requestedFormCompletionUpdated.ts
+++ extensions/healthie/webhooks/requestedFormCompletionUpdated.ts
@@ -3,7 +3,20 @@ import {
   type DataPointDefinition,
   type Webhook,
 } from '@awell-health/extensions-core'
-import { type HealthieWebhookPayload } from '../lib/types'
+import { HEALTHIE_IDENTIFIER, type HealthieWebhookPayload } from '../lib/types'
+import z from 'zod'
+import { validateWebhookPayloadAndCreateSdk } from '../lib/sdk/validatePayloadAndCreateSdk'
+import { type settings } from '../settings'
+
+const payloadSchema = z
+  .object({
+    resource_id: z.string(),
+  })
+  .transform((data) => {
+    return {
+      updatedFormCompletionId: data.resource_id,
+    }
+  })
 
 const dataPoints = {
   updatedFormCompletionId: {
@@ -14,24 +27,33 @@ const dataPoints = {
 
 export const requestFormCompletionUpdated: Webhook<
   keyof typeof dataPoints,
-  HealthieWebhookPayload
+  HealthieWebhookPayload,
+  typeof settings
 > = {
   key: 'requestFormCompletionUpdated',
   dataPoints,
   onWebhookReceived: async ({ payload, settings }, onSuccess, onError) => {
-    const { resource_id: updatedFormCompletionId } = payload
-
-    if (isNil(updatedFormCompletionId)) {
-      await onError({
-        // We should automatically send a 400 here, so no need to provide info
-      })
-    } else {
-      await onSuccess({
-        data_points: {
-          updatedFormCompletionId,
+    const {
+      validatedPayload: { updatedFormCompletionId },
+      sdk,
+    } = await validateWebhookPayloadAndCreateSdk({
+      payloadSchema,
+      payload,
+      settings,
+    })
+    const response = await sdk.GetRequestedFormCompletion({ id: updatedFormCompletionId })
+    const healthiePatientId = response?.data?.requestedFormCompletion?.recipient_id
+    await onSuccess({
+      data_points: {
+        updatedFormCompletionId,
+      },
+      ...(!isNil(healthiePatientId) && {
+        patient_identifier: {
+          system: HEALTHIE_IDENTIFIER,
+          value: healthiePatientId,
         },
-      })
-    }
+      }),
+    })
   },
 }
 
