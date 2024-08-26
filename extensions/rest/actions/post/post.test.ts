43ef2394b7ec1d0ed7f323af6be32511039874bc
Nick Hellemans <nick@awellhealth.com>
2024-08-26 21:50:30 +0200

    chore(rest): add basic error handling

--- extensions/rest/actions/post/post.test.ts
+++ extensions/rest/actions/post/post.test.ts
@@ -17,11 +17,16 @@ describe('REST - POST', () => {
   test('Should work', async () => {
     ;(global.fetch as jest.Mock).mockResolvedValue({
       status: 200,
+      statusText: 'Ok',
+      ok: true,
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
       json: jest.fn().mockResolvedValue({
         success: true,
         message: 'Data received successfully',
       }),
-    })
+    } satisfies Partial<Response>)
 
     const jsonPayload = {
       'key-1': 'value',
@@ -68,6 +73,61 @@ describe('REST - POST', () => {
         response: '{"success":true,"message":"Data received successfully"}',
         statusCode: '200',
       },
+    })
+  })
+
+  test('Should catch errors', async () => {
+    ;(global.fetch as jest.Mock).mockResolvedValue({
+      status: 404,
+      ok: false,
+      statusText: 'Not found',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      json: jest.fn().mockResolvedValue({
+        message: 'Not found',
+      }),
+    } satisfies Partial<Response>)
+
+    const mockOnActivityCreateParams = generateTestPayload({
+      fields: {
+        endpoint: 'https://webhook.site/bb853fec-9260-44e5-a944-17894d678a7f',
+        headers: '{}',
+        jsonPayload: `{}`,
+        additionalPayload: `{}`,
+      },
+      settings: {},
+    })
+
+    await post.onActivityCreated(
+      mockOnActivityCreateParams,
+      onComplete,
+      onError
+    )
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      'https://webhook.site/bb853fec-9260-44e5-a944-17894d678a7f',
+      expect.objectContaining({
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+      })
+    )
+
+    expect(onError).toHaveBeenCalledWith({
+      events: [
+        expect.objectContaining({
+          error: {
+            category: 'SERVER_ERROR',
+            message: '404 Not found ',
+          },
+          text: {
+            en: '404 Not found ',
+          },
+        }),
+      ],
     })
   })
 })
