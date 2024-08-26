f0815acd577de41cfb584659e1483bbd53cbb40c
Nick Hellemans <nick@awellhealth.com>
2024-08-26 16:54:10 +0200

    fix(rest): handle non-json response

--- extensions/rest/actions/post/post.ts
+++ extensions/rest/actions/post/post.ts
@@ -38,12 +38,25 @@ export const post: Action<
       }),
     })
 
-    const responseBody = await response.json()
+    const getResponseBody = async (
+      response: Response
+    ): Promise<string | object> => {
+      const contentType = response.headers.get('content-type') ?? ''
+      if (contentType?.toLowerCase().includes('application/json')) {
+        return await response.json()
+      }
+      return await response.text()
+    }
+
+    const responseBody = await getResponseBody(response)
     const statusCode = response.status
 
     await onComplete({
       data_points: {
-        response: JSON.stringify(responseBody),
+        response:
+          typeof responseBody === 'string'
+            ? responseBody
+            : JSON.stringify(responseBody),
         statusCode: String(statusCode),
       },
     })
