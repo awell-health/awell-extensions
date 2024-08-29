88a4253711edde03ffdbcdf9f42a910f8ac069d4
JB <jonathan@awellhealth.com>
2024-08-29 05:34:49 -0700

    Update extensions/workramp/index.ts
    Co-authored-by: Nick Hellemans <nick@awellhealth.com>

--- extensions/workramp/index.ts
+++ extensions/workramp/index.ts
@@ -9,7 +9,7 @@ export const Workramp: Extension = {
   key: 'workramp',
   title: 'Workramp',
   icon_url:
-    'https://www.workramp.com/wp-content/themes/workramp/images/logo-workramp.svg',
+    'https://res.cloudinary.com/da7x4rzl4/image/upload/v1724917358/Awell%20Extensions/workramp_logo.jpg',
   description:
     'An LMS platform. The system identifier used is https://www.workramp.com/',
   category: Category.CONTENT_AND_FILES,
