07bfd057e38fd8fdd4a8ec3d39effac4a5b2bf8f
Nick Hellemans <nick@awellhealth.com>
2024-09-06 14:14:44 +0200

    fix(rest): post response reader

--- extensions/rest/actions/post/post.test.ts
+++ extensions/rest/actions/post/post.test.ts
@@ -14,7 +14,7 @@ describe('REST - POST', () => {
     jest.restoreAllMocks()
   })
 
-  test('Should work', async () => {
+  test('Should work with JSON response', async () => {
     ;(global.fetch as jest.Mock).mockResolvedValue({
       status: 200,
       statusText: 'Ok',
@@ -22,10 +22,12 @@ describe('REST - POST', () => {
       headers: new Headers({
         'Content-Type': 'application/json',
       }),
-      json: jest.fn().mockResolvedValue({
-        success: true,
-        message: 'Data received successfully',
-      }),
+      text: jest.fn().mockResolvedValue(
+        JSON.stringify({
+          success: true,
+          message: 'Data received successfully',
+        })
+      ),
     } satisfies Partial<Response>)
 
     const jsonPayload = {
@@ -76,6 +78,65 @@ describe('REST - POST', () => {
     })
   })
 
+  test('Should work with non)JSON response', async () => {
+    ;(global.fetch as jest.Mock).mockResolvedValue({
+      status: 200,
+      statusText: 'Ok',
+      ok: true,
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      text: jest.fn().mockResolvedValue(undefined),
+    } satisfies Partial<Response>)
+
+    const jsonPayload = {
+      'key-1': 'value',
+    }
+    const additionalPayload = {
+      'key-2': 'value',
+    }
+
+    const mockOnActivityCreateParams = generateTestPayload({
+      fields: {
+        endpoint: 'https://webhook.site/bb853fec-9260-44e5-a944-17894d678a7f',
+        headers: JSON.stringify({
+          Authorization: 'Bearer key',
+        }),
+        jsonPayload: JSON.stringify(jsonPayload),
+        additionalPayload: JSON.stringify(additionalPayload),
+      },
+      settings: {},
+    })
+
+    await post.onActivityCreated(
+      mockOnActivityCreateParams,
+      onComplete,
+      onError
+    )
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      'https://webhook.site/bb853fec-9260-44e5-a944-17894d678a7f',
+      expect.objectContaining({
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer key',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...jsonPayload,
+          ...additionalPayload,
+        }),
+      })
+    )
+
+    expect(onComplete).toHaveBeenCalledWith({
+      data_points: {
+        response: 'No content',
+        statusCode: '200',
+      },
+    })
+  })
+
   test('Should catch errors', async () => {
     ;(global.fetch as jest.Mock).mockResolvedValue({
       status: 404,
@@ -84,9 +145,11 @@ describe('REST - POST', () => {
       headers: new Headers({
         'Content-Type': 'application/json',
       }),
-      json: jest.fn().mockResolvedValue({
-        message: 'Not found',
-      }),
+      text: jest.fn().mockResolvedValue(
+        JSON.stringify({
+          message: 'Not found',
+        })
+      ),
     } satisfies Partial<Response>)
 
     const mockOnActivityCreateParams = generateTestPayload({
