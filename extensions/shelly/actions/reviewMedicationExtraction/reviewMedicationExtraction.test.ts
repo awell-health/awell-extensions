8ba4a24b7ba49c633c6ceff0d4bb697c19bb87c4
Nick Hellemans <nick@awellhealth.com>
2024-09-18 14:43:57 +0200

    feat(shelly): review medication

---
+++ extensions/shelly/actions/reviewMedicationExtraction/reviewMedicationExtraction.test.ts
@@ -0,0 +1,33 @@
+import { TestHelpers } from '@awell-health/extensions-core'
+import { generateTestPayload } from '@/tests'
+import { reviewMedicationExtraction } from '.'
+
+describe('Shelly - Review medication extraction', () => {
+  const { onComplete, onError, helpers, extensionAction, clearMocks } =
+    TestHelpers.fromAction(reviewMedicationExtraction)
+
+  beforeEach(() => {
+    clearMocks()
+  })
+
+  test('Should work', async () => {
+    await extensionAction.onEvent({
+      payload: generateTestPayload({
+        fields: {
+          imageUrl:
+            'https://res.cloudinary.com/da7x4rzl4/image/upload/v1726601981/hackathon-sep-2024/seqn5izsagvs5nlferlf.png',
+          medicationData: JSON.stringify({}),
+        },
+        settings: {
+          openAiApiKey: 'a',
+        },
+      }),
+      onComplete,
+      onError,
+      helpers,
+    })
+
+    expect(onError).not.toHaveBeenCalled()
+    expect(onComplete).not.toHaveBeenCalled()
+  })
+})
---
+++ extensions/shelly/actions/reviewMedicationExtraction/reviewMedicationExtraction.ts
@@ -0,0 +1,33 @@
+// import { AwellSdk } from '@awell-health/awell-sdk'
+import { Category, type Action } from '@awell-health/extensions-core'
+import { validatePayloadAndCreateSdk } from '../../lib'
+import { type settings } from '../../settings'
+import { fields, dataPoints, FieldsValidationSchema } from './config'
+
+export const reviewMedicationExtraction: Action<
+  typeof fields,
+  typeof settings,
+  keyof typeof dataPoints
+> = {
+  key: 'reviewMedicationExtraction',
+  category: Category.WORKFLOW,
+  title: 'Review medication extraction',
+  description: 'Allow a stakeholder to review the medication extraction',
+  fields,
+  previewable: false,
+  dataPoints,
+  options: {
+    stakeholders: {
+      label: 'Stakeholder',
+      mode: 'single',
+    },
+  },
+  onEvent: async ({ payload, onComplete, onError, helpers }): Promise<void> => {
+    await validatePayloadAndCreateSdk({
+      fieldsSchema: FieldsValidationSchema,
+      payload,
+    })
+
+    // Completion in Hosted Pages
+  },
+}
