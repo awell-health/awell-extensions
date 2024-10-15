723faecffe1aeb7a621ecd8a6c3e190ce232a36e
mohsinht <mohsinhayat104@gmail.com>
2024-10-15 16:18:03 +0500

    fix(healthie): retry getSetPasswordLink using exponential backoff in case of null response

--- extensions/healthie/actions/getSetPasswordLink/getSetPasswordLink.test.ts
+++ extensions/healthie/actions/getSetPasswordLink/getSetPasswordLink.test.ts
@@ -6,6 +6,7 @@ import { getSetPasswordLink as actionInterface } from '.'
 jest.mock('@extensions/healthie/lib/sdk/genql', () => ({
   HealthieSdk: jest.fn().mockImplementation(() => ({
     client: {
+      mutation: jest.fn(),
       query: jest.fn().mockResolvedValue({
         user: {
           set_password_link:
@@ -19,6 +20,8 @@ jest.mock('@extensions/healthie/lib/sdk/genql', () => ({
 const mockedHealthieSdk = jest.mocked(HealthieSdk)
 
 describe('Healthie - Get password link', () => {
+  let mockedClient: any
+
   const {
     extensionAction: action,
     onComplete,
@@ -29,6 +32,10 @@ describe('Healthie - Get password link', () => {
 
   beforeEach(() => {
     clearMocks()
+    mockedClient = {
+      query: jest.fn(),
+      mutation: jest.fn(),
+    }
   })
 
   test('Should call onComplete', async () => {
@@ -49,6 +56,47 @@ describe('Healthie - Get password link', () => {
     })
 
     expect(mockedHealthieSdk).toHaveBeenCalled()
+    expect(onComplete).toHaveBeenCalledWith({
+      data_points: {
+        setPasswordLink:
+          'https://securestaging.gethealthie.com/set_initial_password?signup_token=some-token',
+      },
+    })
+  })
+
+  test('Should retry if set_password_link is null and succeed after retries', async () => {
+    // Mock the SDK to return null for the first two attempts, and succeed on the third attempt
+    mockedClient.query
+      .mockResolvedValueOnce({ user: { set_password_link: null } }) // First attempt
+      .mockResolvedValueOnce({ user: { set_password_link: null } }) // Second attempt
+      .mockResolvedValueOnce({
+        user: {
+          set_password_link:
+            'https://securestaging.gethealthie.com/set_initial_password?signup_token=some-token',
+        },
+      }) // Third attempt
+    ;(HealthieSdk as jest.Mock).mockImplementationOnce(() => ({
+      client: mockedClient,
+    }))
+
+    await action.onEvent({
+      payload: {
+        ...testPayload,
+        fields: {
+          healthiePatientId: '453019',
+        },
+        settings: {
+          apiUrl: 'https://staging-api.gethealthie.com/graphql',
+          apiKey: 'apiKey',
+        },
+      },
+      onComplete,
+      onError,
+      helpers,
+    })
+
+    // Ensure the query was called 3 times and succeeded on the third attempt
+    expect(mockedClient.query).toHaveBeenCalledTimes(3)
     expect(onComplete).toHaveBeenCalledWith({
       data_points: {
         setPasswordLink:
--- extensions/healthie/actions/getSetPasswordLink/getSetPasswordLink.ts
+++ extensions/healthie/actions/getSetPasswordLink/getSetPasswordLink.ts
@@ -3,6 +3,7 @@ import { Category } from '@awell-health/extensions-core'
 import { type settings } from '../../settings'
 import { fields, FieldsValidationSchema, dataPoints } from './config'
 import { validatePayloadAndCreateSdk } from '../../lib/sdk/validatePayloadAndCreateSdk'
+import { isNil } from 'lodash'
 
 export const getSetPasswordLink: Action<typeof fields, typeof settings> = {
   key: 'getSetPasswordLink',
@@ -13,27 +14,47 @@ export const getSetPasswordLink: Action<typeof fields, typeof settings> = {
   previewable: false,
   dataPoints,
   onActivityCreated: async (payload, onComplete, onError): Promise<void> => {
+    const MAX_RETRIES = 3
+    const BASE_DELAY_MS = process.env.NODE_ENV === 'test' ? 10 : 1000
+
     const { fields, healthieSdk } = await validatePayloadAndCreateSdk({
       fieldsSchema: FieldsValidationSchema,
       payload,
     })
 
-    const res = await healthieSdk.client.query({
-      user: {
-        __args: {
-          id: fields.healthiePatientId,
+    let setPasswordLink: string | undefined
+
+    /**
+     * We need to retry the API call because:
+     * - The setPasswordLink field is not immediately available after creating a new user.
+     * - The API response is inconsistent
+     */
+    for (let attempt = 0; attempt < MAX_RETRIES; attempt++) {
+      const res = await healthieSdk.client.query({
+        user: {
+          __args: {
+            id: fields.healthiePatientId,
+          },
+          set_password_link: true,
         },
-        set_password_link: true,
-      },
-    })
+      })
+
+      if (!isNil(res.user?.set_password_link)) {
+        setPasswordLink = String(res.user?.set_password_link)
+        break
+      }
+
+      // Exponential backoff delay calculation
+      const delay = BASE_DELAY_MS * Math.pow(2, attempt)
+      await new Promise((resolve) => setTimeout(resolve, delay))
+    }
 
     await onComplete({
       data_points: {
         setPasswordLink:
-          res.user?.set_password_link === undefined ||
-          res.user?.set_password_link === null
+          setPasswordLink === undefined || setPasswordLink === null
             ? undefined
-            : String(res.user.set_password_link),
+            : String(setPasswordLink),
       },
     })
   },
