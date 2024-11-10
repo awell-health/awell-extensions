df2e910d11522c6e039a494df31d16e50bc95118
Nick Hellemans <nick@awellhealth.com>
2024-11-10 14:27:05 +0100

    docs(): more verbose docs

--- extensions/bland/actions/sendCallWithPathway/config/dataPoints.ts
+++ extensions/bland/actions/sendCallWithPathway/config/dataPoints.ts
@@ -3,6 +3,7 @@ import { type DataPointDefinition } from '@awell-health/extensions-core'
 export const dataPoints = {
   call_id: {
     // This key cannot be changed as it will break the integration!
+    // Needs to match exactly with the key in the payload sent by Bland
     key: 'call_id',
     valueType: 'string',
   },
