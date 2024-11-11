6389caa3de6718fb63780fa61d5d304a549441bf
Abigaila Ekiert <44117299+sharlotta93@users.noreply.github.com>
2024-11-11 21:30:30 +0100

    add history items to elation actions (#507)
    * add history items to elation actions
    
    * fix build
    
    * fix build
    
    * remove redundant try/catch

---
+++ extensions/elation/actions/addHistories/addHistories.ts
@@ -0,0 +1,65 @@
+import { z } from 'zod'
+import { AxiosError } from 'axios'
+import { isNil } from 'lodash'
+
+import { type Action, Category, validate } from '@awell-health/extensions-core'
+import { SettingsValidationSchema, type settings } from '../../settings'
+import { makeAPIClient } from '../../client'
+import { FieldsValidationSchema, fields as elationFields } from './config'
+import { type HistoryTypeSchema } from '../../types/history'
+
+export const addHistories: Action<typeof elationFields, typeof settings> = {
+  key: 'addHistories',
+  category: Category.EHR_INTEGRATIONS,
+  title: 'Add Histories',
+  description: "Add history items on Elation's patient page",
+  fields: elationFields,
+  previewable: true,
+  onEvent: async ({ payload, onComplete }): Promise<void> => {
+    const { fields, settings } = validate({
+      schema: z.object({
+        fields: FieldsValidationSchema,
+        settings: SettingsValidationSchema,
+      }),
+      payload,
+    })
+    // we can execute up to 12 time so we want to capture if any of the fields failed
+    const failedFields: string[] = []
+
+    const api = makeAPIClient(settings)
+
+    // Get all field names except 'patientId'
+    const fieldNames = Object.keys(fields).filter(
+      (field) => field !== 'patientId'
+    )
+
+    // Attempt to add histories for each field
+    await Promise.all(
+      fieldNames.map(async (field) => {
+        const key = field as keyof typeof elationFields
+        const type = elationFields[key]?.label as z.infer<
+          typeof HistoryTypeSchema
+        >
+
+        try {
+          await api.addHistory({
+            type,
+            patient: fields.patientId,
+            text: field,
+          })
+        } catch (failedField) {
+          const errMessage =
+            failedField instanceof Error ? failedField.message : 'failed to add'
+          failedFields.push(`${type}: ${errMessage}`)
+        }
+      })
+    )
+
+    if (failedFields.length > 0) {
+      throw new Error(
+        `Failed to create histories for fields: ${failedFields.join(', ')}`
+      )
+    }
+    await onComplete()
+  },
+}
