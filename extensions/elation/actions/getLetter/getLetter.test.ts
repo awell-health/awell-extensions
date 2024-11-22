590e72d9223eddaf6f975cf7e9c0ca0d8fa177ee
Nick Hellemans <nick@awellhealth.com>
2024-11-22 14:23:20 +0100

    feat(): get letter (#526)

---
+++ extensions/elation/actions/getLetter/getLetter.test.ts
@@ -0,0 +1,53 @@
+import { makeAPIClientMockFunc } from '../../__mocks__/client'
+import { makeAPIClient } from '../../client'
+import { getLetter as action } from './getLetter'
+import { TestHelpers } from '@awell-health/extensions-core'
+
+jest.mock('../../client')
+
+describe('Elation - Get letter', () => {
+  const {
+    extensionAction: sendCall,
+    onComplete,
+    onError,
+    helpers,
+    clearMocks,
+  } = TestHelpers.fromAction(action)
+
+  beforeAll(() => {
+    const mockAPIClient = makeAPIClient as jest.Mock
+    mockAPIClient.mockImplementation(makeAPIClientMockFunc)
+  })
+
+  beforeEach(() => {
+    clearMocks()
+    jest.clearAllMocks()
+  })
+
+  test('Should return the correct letter', async () => {
+    await sendCall.onEvent({
+      payload: {
+        fields: {
+          letterId: 123,
+        },
+        settings: {
+          client_id: 'clientId',
+          client_secret: 'clientSecret',
+          username: 'username',
+          password: 'password',
+          auth_url: 'authUrl',
+          base_url: 'baseUrl',
+        },
+      } as any,
+      onComplete,
+      onError,
+      helpers,
+    })
+
+    expect(onComplete).toHaveBeenCalledWith({
+      data_points: {
+        body: 'Test Body Message',
+      },
+    })
+  })
+})
---
+++ extensions/elation/actions/getLetter/getLetter.ts
@@ -0,0 +1,30 @@
+import { type Action, Category } from '@awell-health/extensions-core'
+import { type settings } from '../../settings'
+import { makeAPIClient } from '../../client'
+import { FieldsValidationSchema, fields, dataPoints } from './config'
+
+export const getLetter: Action<
+  typeof fields,
+  typeof settings,
+  keyof typeof dataPoints
+> = {
+  key: 'getLetter',
+  category: Category.EHR_INTEGRATIONS,
+  title: 'Get letter',
+  description: 'Retrieve a letter from Elation.',
+  fields,
+  previewable: true,
+  dataPoints,
+  onActivityCreated: async (payload, onComplete, onError): Promise<void> => {
+    const { letterId } = FieldsValidationSchema.parse(payload.fields)
+    const api = makeAPIClient(payload.settings)
+
+    const res = await api.getLetter(letterId)
+
+    await onComplete({
+      data_points: {
+        body: res.body,
+      },
+    })
+  },
+}
