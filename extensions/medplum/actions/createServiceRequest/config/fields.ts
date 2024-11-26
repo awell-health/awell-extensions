550ccb00afab4c674640b0ac150a8475f9f6173c
JB <jonathan@awellhealth.com>
2024-11-26 08:37:19 -0800

    feat(medplum): updated createServiceRequest to dropdown

--- extensions/medplum/actions/createServiceRequest/config/fields.ts
+++ extensions/medplum/actions/createServiceRequest/config/fields.ts
@@ -1,6 +1,9 @@
 import { FieldType, type Field } from '@awell-health/extensions-core'
 import { type ZodObject, type ZodTypeAny } from 'zod'
 import { CreateServiceRequestSchema } from '../../../fhir/schemas/resources/serviceRequest.schema'
+import { ServiceRequestStatusSchema } from '../../../fhir/schemas/atoms/StatusSchema'
+import { ServiceRequestIntentSchema } from '../../../fhir/schemas/atoms/IntentSchema'
+import { PrioritySchema } from 'extensions/medplum/fhir/schemas/atoms/PrioritySchema'
 
 export const fields = {
   patientId: {
@@ -17,6 +20,14 @@ export const fields = {
       'The status of the order. Allowed values: draft | active | on-hold | revoked | completed | entered-in-error | unknown',
     type: FieldType.STRING,
     required: true,
+    options: {
+      dropdownOptions: Object.values(ServiceRequestStatusSchema.enum).map(
+        (status) => ({
+          label: status,
+          value: status,
+        })
+      ),
+    },
   },
   intent: {
     id: 'intent',
@@ -25,6 +36,14 @@ export const fields = {
       'Whether the request is a proposal, plan, an original order or a reflex order. Allowed values: proposal | plan | directive | order | original-order | reflex-order | filler-order | instance-order | option',
     type: FieldType.STRING,
     required: true,
+    options: {
+      dropdownOptions: Object.values(ServiceRequestIntentSchema.enum).map(
+        (intent) => ({
+          label: intent,
+          value: intent,
+        })
+      ),
+    },
   },
   priority: {
     id: 'priority',
@@ -32,6 +51,12 @@ export const fields = {
     description: 'Allowed values: routine | urgent | asap | stat',
     type: FieldType.STRING,
     required: false,
+    options: {
+      dropdownOptions: Object.values(PrioritySchema.enum).map((priority) => ({
+        label: priority,
+        value: priority,
+      })),
+    },
   },
 } satisfies Record<string, Field>
 
