e88560fd1accf44921fde544184354968395b257
Etienne Bömcke <etienne@awellhealth.com>
2024-12-16 10:56:17 +0100

    chore(elation): handle review suggestions

--- extensions/elation/actions/closeCareGap/closeCareGap.ts
+++ extensions/elation/actions/closeCareGap/closeCareGap.ts
@@ -1,6 +1,7 @@
-import { Category, type Action } from '@awell-health/extensions-core'
+import { Category, validate, type Action } from '@awell-health/extensions-core'
+import { z } from 'zod'
 import { makeAPIClient } from '../../client'
-import { type settings } from '../../settings'
+import { SettingsValidationSchema, type settings } from '../../settings'
 import { dataPoints, fields, FieldsValidationSchema } from './config'
 
 export const closeCareGap: Action<
@@ -15,13 +16,23 @@ export const closeCareGap: Action<
   fields,
   previewable: true,
   dataPoints,
-  onActivityCreated: async (payload, onComplete, onError): Promise<void> => {    
-    const { quality_program, caregap_id } = FieldsValidationSchema.parse(payload.fields)
+  onActivityCreated: async (payload, onComplete, onError): Promise<void> => {
+    const {
+      fields: { quality_program, caregap_id },
+      settings,
+    } = validate({
+      schema: z.object({
+        fields: FieldsValidationSchema,
+        settings: SettingsValidationSchema,
+      }),
+      payload,
+    })
 
     // The care gap API uses a different base URL than the rest of the Elation API, see https://docs.elationhealth.com/reference/caregaps_post_caregaps_api__quality_program__caregap__post-1
-    const baseUrl = payload.settings.base_url ?? ''
-    const caregapBaseUrl = baseUrl
-      .replace(/(.+\.)?elationemr\.com\/api\/2\.0\/?/, 'caregaps.$1elationemr.com/caregaps/api/')
+    const caregapBaseUrl = settings.base_url.replace(
+      /(.+\.)?elationemr\.com\/api\/2\.0\/?/,
+      'caregaps.$1elationemr.com/caregaps/api/'
+    )
 
     const api = makeAPIClient({
       ...payload.settings,
--- extensions/elation/actions/createCareGap/createCareGap.ts
+++ extensions/elation/actions/createCareGap/createCareGap.ts
@@ -1,6 +1,7 @@
-import { Category, type Action } from '@awell-health/extensions-core'
+import { Category, validate, type Action } from '@awell-health/extensions-core'
+import { z } from 'zod'
 import { makeAPIClient } from '../../client'
-import { type settings } from '../../settings'
+import { SettingsValidationSchema, type settings } from '../../settings'
 import { dataPoints, fields, FieldsValidationSchema } from './config'
 
 export const createCareGap: Action<
@@ -13,15 +14,33 @@ export const createCareGap: Action<
   title: 'Create care gap',
   description: 'Create a care gap in Elation.',
   fields,
-  previewable: true,
+  previewable: false,
   dataPoints,
-  onActivityCreated: async (payload, onComplete, onError): Promise<void> => {    
-    const { patient_id, practice_id, created_date, status, detail, quality_program, definition_id } = FieldsValidationSchema.parse(payload.fields)
+  onActivityCreated: async (payload, onComplete, onError): Promise<void> => {
+    const {
+      fields: {
+        patient_id,
+        practice_id,
+        created_date,
+        status,
+        detail,
+        quality_program,
+        definition_id,
+      },
+      settings,
+    } = validate({
+      schema: z.object({
+        fields: FieldsValidationSchema,
+        settings: SettingsValidationSchema,
+      }),
+      payload,
+    })
 
     // The care gap API uses a different base URL than the rest of the Elation API, see https://docs.elationhealth.com/reference/caregaps_post_caregaps_api__quality_program__caregap__post-1
-    const baseUrl = payload.settings.base_url ?? ''
-    const caregapBaseUrl = baseUrl
-      .replace(/(.+\.)?elationemr\.com\/api\/2\.0\/?/, 'caregaps.$1elationemr.com/caregaps/api/')
+    const caregapBaseUrl = settings.base_url.replace(
+      /(.+\.)?elationemr\.com\/api\/2\.0\/?/,
+      'caregaps.$1elationemr.com/caregaps/api/'
+    )
 
     const api = makeAPIClient({
       ...payload.settings,
