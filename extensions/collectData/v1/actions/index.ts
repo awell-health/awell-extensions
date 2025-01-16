53d4b7ab216579b55ff9611ef02eed1385b6b468
mohsinht <mohsinhayat104@gmail.com>
2025-01-17 00:32:08 +0500

    feat(collectData): enable arrayTest action in CollectData extension

--- extensions/collectData/v1/actions/index.ts
+++ extensions/collectData/v1/actions/index.ts
@@ -1,3 +1,3 @@
 export { remoteSingleSelect } from './remoteSingleSelect'
-// export { arrayTest } from './arrayTest'
+export { arrayTest } from './arrayTest'
 export { collectMedication } from './collectMedication'
