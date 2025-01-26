2664157d8605e1ea9ae8f7f4422f4610a6d8a546
belmai <ibrahimovic.belma@gmail.com>
2025-01-26 09:54:49 +0100

    fix(elation): sending ' ' for clearing tags raises an error - lets' try with empty array again.

--- extensions/elation/actions/updatePatientTags/updateTags.ts
+++ extensions/elation/actions/updatePatientTags/updateTags.ts
@@ -3,8 +3,7 @@ import { type ElationAPIClient } from '../../client'
 export const updateElationTags = async (api: ElationAPIClient, patientId: number, tags: string[]): Promise<void> => {
     if (tags.length === 0) {
       await api.updatePatient(patientId, {
-        // @ts-expect-error - elation api does not clear tags on an empty array
-        tags: ' ',
+        tags: [],
       })
     } else {
       await api.updatePatient(patientId, {
