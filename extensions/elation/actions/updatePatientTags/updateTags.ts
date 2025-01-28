84a8b19d139bcd182137e82ace52d3d99133be63
belmai <ibrahimovic.belma@gmail.com>
2025-01-28 08:59:58 +0100

    chor(elation): update comment about clearing patient tags

--- extensions/elation/actions/updatePatientTags/updateTags.ts
+++ extensions/elation/actions/updatePatientTags/updateTags.ts
@@ -3,8 +3,8 @@ import { type ElationAPIClient } from '../../client'
 export const updateElationTags = async (api: ElationAPIClient, patientId: number, tags: string[]): Promise<void> => {
     if (tags.length === 0) {
       await api.updatePatient(patientId, {
-        // Note: Empty array doesn't clear tags, but [''] does by setting '' as a tag and clearing the rest
-        // TODO: Check the best approach with Elation since this does not clear the tags but sets '' as a tag
+        // Note: Empty array doesn't clear tags, but [''] does by setting '' as a tag and clearing the rest (so it is not actually removing all tags but setting '' as a tag)
+        // This is also recommended workaround Elation team for clearing tags via API
         tags: [''],
       })
     } else {
