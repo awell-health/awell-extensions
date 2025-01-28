f0df5a0fe3c995d4bcc565cd8d14cb99faf99b9f
Nick Hellemans <nick@awellhealth.com>
2025-01-28 09:43:25 +0100

    feat(medication): pretty text output

--- extensions/collectData/v1/actions/collectMedication/config/datapoints.ts
+++ extensions/collectData/v1/actions/collectMedication/config/datapoints.ts
@@ -1,12 +1,19 @@
 import { type DataPointDefinition } from '@awell-health/extensions-core'
 
 export const dataPoints = {
+  // JSON object
   medicationData: {
     key: 'medicationData',
     valueType: 'json',
   },
+  // Serialized JSON object
   medicationDataString: {
     key: 'medicationDataString',
+    valueType: 'string',
+  },
+  // Pretty text output
+  prettyMedicationData: {
+    key: 'prettyMedicationData',
     valueType: 'string',
   },
 } satisfies Record<string, DataPointDefinition>
