c69ad4ce5042e009fdd1ebf9aa1771f52e674b95
JB <jonathan@awellhealth.com>
2025-02-12 14:46:50 -0800

    chore(calendly): update calendly logo to svg

--- extensions/calendly/index.ts
+++ extensions/calendly/index.ts
@@ -7,7 +7,7 @@ export const Calendly: Extension = {
   key: 'calendly',
   title: 'Calendly',
   icon_url:
-    'https://ww2.freelogovectors.net/wp-content/uploads/2023/05/calendly_logo-freelogovectors.net_.png',
+    'https://asset.cloudinary.com/da7x4rzl4/e3feb49a5ef3ccd2951bf93aea0a1552',
   description:
     '** In beta ** Enable scheduling in your care flows with Calendly.',
   category: Category.SCHEDULING,
