fc88f4f4dde2c7ca20848322764f5bd009c37a90
Abigaila Ekiert <arekiert@gmail.com>
2025-05-08 20:07:21 -0500

    rev-196-add-new-braze-actions

--- extensions/braze/actions/index.ts
+++ extensions/braze/actions/index.ts
@@ -3,6 +3,7 @@ import { scheduleSMS } from './scheduleSMS'
 import { sendEmail } from './sendEmail'
 import { scheduleEmail } from './scheduleEmail'
 import { sendEmailUsingTemplate } from './sendEmailUsingTemplate'
+import { sendEmailWithAttributes } from './sendEmailWithAttributes'
 
 export const actions = {
   sendSMS,
@@ -10,4 +11,5 @@ export const actions = {
   sendEmail,
   scheduleEmail,
   sendEmailUsingTemplate,
+  sendEmailWithAttributes,
 }
---
+++ extensions/braze/actions/sendEmailWithAttributes/sendEmailWithAttributes.ts
@@ -0,0 +1,79 @@
+import { z } from 'zod'
+import { isNil } from 'lodash'
+import { Category, validate, type Action } from '@awell-health/extensions-core'
+
+import { type settings, SettingsValidationSchema } from '../../settings'
+import { BrazeClient } from '../../client'
+import { fields, dataPoints, FieldsSchema } from './config'
+
+export const sendEmailWithAttributes = {
+  key: 'sendEmailWithAttributes',
+  title: 'Send Email With Attributes',
+  description: 'Send an email with attributes to a patient.',
+  category: Category.COMMUNICATION,
+  fields,
+  dataPoints,
+  previewable: false,
+  onEvent: async ({ payload, onComplete, onError, helpers }) => {
+    const {
+      fields: {
+        externalUserId: external_user_id,
+        sendId: send_id,
+        email,
+        campaignId: campaign_id,
+        triggerProperties: trigger_properties,
+        attributes,
+      },
+      settings: { apiKey, baseUrl },
+    } = validate({
+      schema: z.object({
+        fields: FieldsSchema,
+        settings: SettingsValidationSchema,
+      }),
+      payload,
+    })
+
+    if (isNil(external_user_id) && isNil(email)) {
+      throw new Error('Either externalUserId or email must be provided.')
+    }
+
+    const client = new BrazeClient({ apiKey, baseUrl })
+
+    const requestBody = {
+      campaign_id,
+      recipients: [
+        {
+          ...(!isNil(external_user_id) && {
+            external_user_id,
+          }),
+          ...(!isNil(email) && {
+            email,
+          }),
+          ...(!isNil(trigger_properties) && {
+            trigger_properties,
+          }),
+          ...(!isNil(attributes) && {
+            attributes,
+          }),
+        },
+      ],
+      ...(!isNil(send_id) && {
+        send_id,
+      }),
+    }
+
+    const resp = await client.sendEmailWithAttributes(requestBody)
+
+    await onComplete({
+      events: [
+        {
+          date: new Date().toISOString(),
+          text: { en: `Email sent. Response: ${JSON.stringify(resp)}` },
+        },
+      ],
+      data_points: {
+        EmailDispatchId: resp.dispatch_id,
+      },
+    })
+  },
+} satisfies Action<typeof fields, typeof settings>
