da288af96efc052aac2bcbfe8151d2d2899e6c6f
Nick Hellemans <nick@awellhealth.com>
2025-07-07 22:25:02 +0200

    chore(freshdesk): add comments on webhook payload

--- extensions/freshdesk/webhooks/TicketCreated/types.ts
+++ extensions/freshdesk/webhooks/TicketCreated/types.ts
@@ -1,1 +1,16 @@
+/**
+ * Problem:
+ * Freshdesk allows their users to define a custom webhook payload.
+ * This means there's no "standard" webhook payload and it's not possible to define a schema for it.
+ *
+ * Alternative 1:
+ * If a customer wants to use a webhook-based integration with Freshdesk, we register the webhook
+ * in a private extension and define a custom contract and schema together with the customer and use that instead.
+ *
+ * Alternative 2:
+ * We try to define a schema for the webhook payload that is flexible enough to handle all possible payloads.
+ * Haven't attempted this yet.
+ *
+ * See https://support.freshdesk.com/support/solutions/articles/132589-using-webhooks-in-automation-rules
+ */
 export type TicketCreatedWebhookPayload = Record<string, unknown>
