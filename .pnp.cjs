0fe9068002a7227705f47a7c103ef6ac44709f7a
JB <jonathan@awellhealth.com>
2025-09-05 19:21:36 -0700

    AWL-2490: module-federation (#692)
    * build(extensions-core): bump extensions-core to 1.1.1
    
    * feat(module-federation): add new hello world frontend component
    
    * ci: bump version to 2.0.330-beta.1 [skip ci]
    
    * feat(module-federation): components/ entrypoint
    
    * ci: bump version to 2.0.330-beta.2 [skip ci]
    
    * bump(extensions-core): bump to 1.1.3
    
    * ci: bump version to 2.0.331-beta.0 [skip ci]
    
    ---------
    
    Co-authored-by: Awell CI <ci@awellhealth.com>

--- .pnp.cjs
+++ .pnp.cjs
@@ -29,7 +29,7 @@ const RAW_RUNTIME_STATE =
         "packageDependencies": [\
           ["@airtop/sdk", "virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:0.1.44"],\
           ["@awell-health/awell-sdk", "npm:0.1.24"],\
-          ["@awell-health/extensions-core", "virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:1.0.28"],\
+          ["@awell-health/extensions-core", "virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:1.1.3"],\
           ["@awell-health/healthie-sdk", "npm:0.1.1"],\
           ["@dropbox/sign", "npm:1.8.0"],\
           ["@faker-js/faker", "npm:8.4.1"],\
@@ -64,6 +64,7 @@ const RAW_RUNTIME_STATE =
           ["@types/node-fetch", "npm:2.6.11"],\
           ["@types/nodemailer", "npm:6.4.16"],\
           ["@types/pdfmake", "npm:0.2.9"],\
+          ["@types/react", "npm:19.1.12"],\
           ["@types/sanitize-html", "npm:2.13.0"],\
           ["@types/showdown", "npm:2.0.6"],\
           ["@types/uuid", "npm:10.0.0"],\
@@ -427,7 +428,7 @@ const RAW_RUNTIME_STATE =
           ["@awell-health/awell-extensions", "workspace:."],\
           ["@airtop/sdk", "virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:0.1.44"],\
           ["@awell-health/awell-sdk", "npm:0.1.24"],\
-          ["@awell-health/extensions-core", "virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:1.0.28"],\
+          ["@awell-health/extensions-core", "virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:1.1.3"],\
           ["@awell-health/healthie-sdk", "npm:0.1.1"],\
           ["@dropbox/sign", "npm:1.8.0"],\
           ["@faker-js/faker", "npm:8.4.1"],\
@@ -462,6 +463,7 @@ const RAW_RUNTIME_STATE =
           ["@types/node-fetch", "npm:2.6.11"],\
           ["@types/nodemailer", "npm:6.4.16"],\
           ["@types/pdfmake", "npm:0.2.9"],\
+          ["@types/react", "npm:19.1.12"],\
           ["@types/sanitize-html", "npm:2.13.0"],\
           ["@types/showdown", "npm:2.0.6"],\
           ["@types/uuid", "npm:10.0.0"],\
@@ -546,22 +548,23 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@awell-health/extensions-core", [\
-      ["npm:1.0.28", {\
-        "packageLocation": "./.yarn/cache/@awell-health-extensions-core-npm-1.0.28-8d245d3003-cddad5e65d.zip/node_modules/@awell-health/extensions-core/",\
+      ["npm:1.1.3", {\
+        "packageLocation": "./.yarn/cache/@awell-health-extensions-core-npm-1.1.3-fc2e4f1f10-5e207e88cd.zip/node_modules/@awell-health/extensions-core/",\
         "packageDependencies": [\
-          ["@awell-health/extensions-core", "npm:1.0.28"]\
+          ["@awell-health/extensions-core", "npm:1.1.3"]\
         ],\
         "linkType": "SOFT"\
       }],\
-      ["virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:1.0.28", {\
-        "packageLocation": "./.yarn/__virtual__/@awell-health-extensions-core-virtual-9262fd1d2c/0/cache/@awell-health-extensions-core-npm-1.0.28-8d245d3003-cddad5e65d.zip/node_modules/@awell-health/extensions-core/",\
+      ["virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:1.1.3", {\
+        "packageLocation": "./.yarn/__virtual__/@awell-health-extensions-core-virtual-6101aef801/0/cache/@awell-health-extensions-core-npm-1.1.3-fc2e4f1f10-5e207e88cd.zip/node_modules/@awell-health/extensions-core/",\
         "packageDependencies": [\
-          ["@awell-health/extensions-core", "virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:1.0.28"],\
+          ["@awell-health/extensions-core", "virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:1.1.3"],\
           ["@awell-health/awell-sdk", "npm:0.1.24"],\
           ["@langchain/core", "npm:0.3.33"],\
           ["@langchain/openai", "virtual:6d1931a4340173b37cf492f77cb803dda2f92958adb6847175388be3c73eb24be6f6bfd25e0fc0b7ad8dba815a972ad5e9d1f18e67fb58466bb7c99205a9d42e#npm:0.3.0"],\
           ["@types/awell-health__awell-sdk", null],\
           ["@types/json-schema", "npm:7.0.15"],\
+          ["@types/react", "npm:18.3.24"],\
           ["axios", "npm:1.7.7"],\
           ["date-fns", "npm:3.6.0"],\
           ["libphonenumber-js", "npm:1.11.8"],\
@@ -4906,6 +4909,15 @@ const RAW_RUNTIME_STATE =
         "linkType": "HARD"\
       }]\
     ]],\
+    ["@types/prop-types", [\
+      ["npm:15.7.15", {\
+        "packageLocation": "./.yarn/cache/@types-prop-types-npm-15.7.15-cefe16a1fa-31aa2f59b2.zip/node_modules/@types/prop-types/",\
+        "packageDependencies": [\
+          ["@types/prop-types", "npm:15.7.15"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
     ["@types/qs", [\
       ["npm:6.9.16", {\
         "packageLocation": "./.yarn/cache/@types-qs-npm-6.9.16-93d56526da-2e8918150c.zip/node_modules/@types/qs/",\
@@ -4920,6 +4932,25 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./.yarn/cache/@types-range-parser-npm-1.2.7-a83c0b6429-95640233b6.zip/node_modules/@types/range-parser/",\
         "packageDependencies": [\
           ["@types/range-parser", "npm:1.2.7"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
+    ["@types/react", [\
+      ["npm:18.3.24", {\
+        "packageLocation": "./.yarn/cache/@types-react-npm-18.3.24-ffcfaf8702-3f8529961a.zip/node_modules/@types/react/",\
+        "packageDependencies": [\
+          ["@types/react", "npm:18.3.24"],\
+          ["@types/prop-types", "npm:15.7.15"],\
+          ["csstype", "npm:3.1.3"]\
+        ],\
+        "linkType": "HARD"\
+      }],\
+      ["npm:19.1.12", {\
+        "packageLocation": "./.yarn/cache/@types-react-npm-19.1.12-d97634a5ed-c03d595b84.zip/node_modules/@types/react/",\
+        "packageDependencies": [\
+          ["@types/react", "npm:19.1.12"],\
+          ["csstype", "npm:3.1.3"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -7307,6 +7338,15 @@ const RAW_RUNTIME_STATE =
           ["cssstyle", "npm:4.2.1"],\
           ["@asamuzakjp/css-color", "npm:2.8.2"],\
           ["rrweb-cssom", "npm:0.8.0"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
+    ["csstype", [\
+      ["npm:3.1.3", {\
+        "packageLocation": "./.yarn/cache/csstype-npm-3.1.3-e9a1c85013-f593cce41f.zip/node_modules/csstype/",\
+        "packageDependencies": [\
+          ["csstype", "npm:3.1.3"]\
         ],\
         "linkType": "HARD"\
       }]\
