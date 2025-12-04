b638f753f0f1c6185bf86f4627235465ca8b0029
Nick Hellemans <nick@awellhealth.com>
2025-12-04 15:49:44 +0100

    feat(date-ext): get next date occurence (#743)

--- extensions/dateHelpers/actions/index.ts
+++ extensions/dateHelpers/actions/index.ts
@@ -1,7 +1,9 @@
 import { getNextWorkday } from './getNextWorkday'
 import { checkWorkingHours } from './checkWorkingHours'
+import { getNextDateOccurrence } from './getNextDateOccurrence'
 
 export const actions = {
   getNextWorkday,
   checkWorkingHours,
+  getNextDateOccurrence,
 }
