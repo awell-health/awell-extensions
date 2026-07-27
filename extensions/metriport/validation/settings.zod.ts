76a439e5b3dc88fc76e5db86c3440643b0661568
Steve Gardner <stevelikesmusic@users.noreply.github.com>
2026-07-27 21:03:48 +0400

    fix: ease validation on Metriport baseUrl (#806)

--- extensions/metriport/validation/settings.zod.ts
+++ extensions/metriport/validation/settings.zod.ts
@@ -3,7 +3,6 @@ import { z } from 'zod'
 export const settingsSchema = z.object({
   baseUrl: z
     .string({ errorMap: () => ({ message: 'Missing baseUrl' }) })
-    .min(1)
     .optional(),
   apiKey: z.string({ errorMap: () => ({ message: 'Missing apiKey' }) }).min(1),
 })
